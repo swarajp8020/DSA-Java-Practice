76634869937d04088fb2121f0edff13db083323e
SwarajXO <purarkarswaraj@gmail.com>
2025-11-01 21:11:44 +0530

    Practice Q.2

---
+++ JavaPrograms/src/Week1Practice/onetoN.java
@@ -0,0 +1,10 @@
+package Week1Practice;
+
+public class onetoN {
+    public static void main(String[] args) {
+        int n = 15;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}

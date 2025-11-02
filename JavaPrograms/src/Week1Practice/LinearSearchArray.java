9119a6ffea4f44a5e1f268afd950aaafe9f432cc
SwarajXO <purarkarswaraj@gmail.com>
2025-11-02 12:23:13 +0530

    Q.2 LinearSearchArray

---
+++ JavaPrograms/src/Week1Practice/LinearSearchArray.java
@@ -0,0 +1,26 @@
+package Week1Practice;
+//Problem:
+//Given an array of integers and a target value, check if the target exists in the array.
+//If found, print its index. If not found, print “Not found.”
+//
+//Example:
+//Input:
+//arr = {3, 8, 1, 9, 5}
+//target = 9
+//Output:
+//Element found at index 3
+
+public class LinearSearchArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 8, 1, 9, 5};
+        int target = 9;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target){
+                System.out.println("Found index");
+                break;
+            } else {
+                System.out.println("Not Found index");
+            }
+        }
+    }
+}

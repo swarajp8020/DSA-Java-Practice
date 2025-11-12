bf992891073b590740f2362087b0a6cd96a5b726
SwarajXO <purarkarswaraj@gmail.com>
2025-11-12 08:36:53 +0530

    12-11-25 Selection Sort

---
+++ JavaPrograms/src/sorting/selectionSort.java
@@ -0,0 +1,30 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        int[] arr={11,3,45,23,10,5,7};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+        for(int n: arr){
+            System.out.println(n);
+        }
+
+    }
+    static void selectionSort(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (arr[j]<arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}

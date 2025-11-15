2c9c8b70ada5aaed5e18dc4980d1f8c414ef0c71
SwarajXO <purarkarswaraj@gmail.com>
2025-11-15 17:25:57 +0530

    15-11-25 Revision ques-20

--- JavaPrograms/src/Week3Practice/p5.java
+++ JavaPrograms/src/Week3Practice/p5.java
@@ -292,25 +292,43 @@ public class p5 {
 //
 //}
 //
+//public static void main(String[] args) {
+//    int[] arr = {3,1,4,2,5,7};
+//    bubbleSort(arr);
+//    System.out.println(Arrays.toString(arr));
+//}
+//static void bubbleSort(int[] arr){
+//    int n = arr.length;
+//    for (int i = 0; i < n-1; i++) {
+//        for (int j = 0; j < n-i-1; j++) {
+//            if (arr[j] > arr[j+1]){
+//                int temp = arr[j];
+//                arr[j] = arr[j+1];
+//                arr[j+1] = temp;
+//            }
+//        }
+//
+//
+//    }
+//}
 public static void main(String[] args) {
     int[] arr = {3,1,4,2,5,7};
-    bubbleSort(arr);
+    selectionSort(arr);
     System.out.println(Arrays.toString(arr));
 }
-static void bubbleSort(int[] arr){
+static void selectionSort(int[] arr){
     int n = arr.length;
     for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+        int minIndex =i;
+        for (int j = i+1; j < n; j++) {
+            if (arr[j]< arr[minIndex]){
+                minIndex = j;
             }
         }
-
-
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
     }
 }
-
 }
 

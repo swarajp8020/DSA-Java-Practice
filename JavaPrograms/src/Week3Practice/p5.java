5cbdac3598b8f29fb3bc96ba1333b244539de2bc
SwarajXO <purarkarswaraj@gmail.com>
2025-11-15 07:27:28 +0530

    15-11-25 Revision

---
+++ JavaPrograms/src/Week3Practice/p5.java
@@ -0,0 +1,65 @@
+package Week3Practice;
+
+import java.util.Arrays;
+
+public class p5 {
+    //reverse the array, swap elements from both ends moving toward the center until all elements are reversed in place.
+//    static int reverseArray(int[] arr){
+//        int left = 0, right = arr.length-1;
+//
+//        while (left<right){
+//            int temp = arr[right];
+//            arr[right] = arr[left];
+//            arr[left] = temp;
+//            left++;
+//            right--;
+//        }
+//        return left;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = {2,4,5,1,3};
+//        System.out.println(Arrays.toString(arr));
+//        reverseArray(arr);
+//        System.out.println(Arrays.toString(arr));
+//    }
+//    Print 1 to n
+//    public static void main(String[] args) {
+//        int n = 15;
+//        for (int i = 1; i <=n ; i++) {
+//            System.out.print(i+ " ");
+//        }
+//    }
+//    Sum of first n numbers
+
+//    public static void main(String[] args) {
+//        int[] arr = {3,1,2,5,6};
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            sum += arr[i];
+//        }
+//        System.out.println(sum);
+//    }
+
+//Print a triangle pattern (like *, **, ***)
+//public static void main(String[] args) {
+//    int n = 5;
+//    for (int i = 1; i < n; i++) {
+//        for (int j = 0; j < i; j++) {
+//            System.out.print("*");
+//        }
+//        System.out.println();
+//    }
+//}
+    //Reverse a number (e.g., 123 → 321
+public static void main(String[] args) {
+    int num = 123;
+    int rev = 0;
+    while (num>0){
+        int sum = num %10;
+        rev = rev * 10 + sum;
+        num = num/10;
+    }
+    System.out.println(rev);
+}
+}

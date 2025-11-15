4d2d945a267e3a5988aa4e01e62dc4f0b45a2253
SwarajXO <purarkarswaraj@gmail.com>
2025-11-15 07:39:29 +0530

    15-11-25 Revision ques-9

--- JavaPrograms/src/Week3Practice/p5.java
+++ JavaPrograms/src/Week3Practice/p5.java
@@ -91,15 +91,22 @@ public class p5 {
 //}
 
 //    Count digits in a number
-public static void main(String[] args) {
-
-    int num = 1445;
-    int count = 0;
-    while (num>0){
-        num = num/10;
-        count++;
-    }
-    System.out.println(count);
-}
-
+//public static void main(String[] args) {
+//
+//    int num = 1445;
+//    int count = 0;
+//    while (num>0){
+//        num = num/10;
+//        count++;
+//    }
+//    System.out.println(count);
+//}
+//    Print multiplication table of a number
+//public static void main(String[] args) {
+//    int n=5;
+//    for (int i = 1; i <= 10 ; i++) {
+//        System.out.println(n*i);
+//
+//    }
+//}
 }

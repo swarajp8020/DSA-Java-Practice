5ca07ef5012fc55a9c961cfe6420447db2d01a4e
SwarajXO <purarkarswaraj@gmail.com>
2025-11-17 10:48:41 +0530

    17-11-25 quicksort

---
+++ JavaPrograms/src/sorting/quickSort.java
@@ -0,0 +1,35 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class quickSort {
+    public static void main(String[] args) {
+        int[] arr = {9,6,4,2};
+        quickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));;
+    }
+    static void quickSort(int[] arr, int left, int right){
+        if (left>=right){return;}
+        int pivotIndex = partition(arr,left,right);
+        quickSort(arr,left,pivotIndex-1); // left half
+        quickSort(arr,pivotIndex+1, right); // right half
+    }
+
+     static int partition(int[] arr, int left, int right) {
+        int pivot = arr[right]; // pick last element
+        int i = left -1;       // boundary for smaller elements
+         for (int j = left; j < right; j++) {
+             if (arr[j]<=pivot){
+                 i++;
+                 swap(arr,i,j);
+             }
+         }
+         swap(arr,i+1,right);
+         return i+1;  // boundary for smaller elements
+    }
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b]=temp;
+    }
+}

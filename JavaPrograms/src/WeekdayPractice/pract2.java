cc3b03b8d18dd18a66b8742603ca459aaae9dca3
SwarajXO <purarkarswaraj@gmail.com>
2025-11-19 10:14:02 +0530

    19-11-25 MergeSort  Practice

--- JavaPrograms/src/WeekdayPractice/pract2.java
+++ JavaPrograms/src/WeekdayPractice/pract2.java
@@ -42,26 +42,62 @@ public class pract2 {
 //        }
 //        return inversion;
 //    }
-
+//
+//public static void main(String[] args) {
+//    int[] a = {1,3,5,7};
+//    int[] b= {2,4,6,8};
+//    int[] res = merge(a,b);
+//    System.out.println(Arrays.toString(res));
+//    }
+//    static int[] merge(int[]a, int[]b){
+//    int[] temp = new int[a.length+b.length];
+//    int i = 0,j=0,k=0;
+//    while (i<a.length && j<b.length){
+//        if (a[i]<=b[j]){
+//            temp[k++] = a[i++];
+//        }else {
+//            temp[k++] = b[j++];
+//        }
+//    }
+//    while (i<a.length)temp[k++] = a[i++];
+//    while (j<b.length)temp[k++] = b[j++];
+//
+//    return temp;
+//    }
+    //MergeSort
 public static void main(String[] args) {
-    int[] a = {1,3,5,7};
-    int[] b= {2,4,6,8};
-    int[] res = merge(a,b);
-    System.out.println(Arrays.toString(res));
-    }
-    static int[] merge(int[]a, int[]b){
-    int[] temp = new int[a.length+b.length];
-    int i = 0,j=0,k=0;
-    while (i<a.length && j<b.length){
-        if (a[i]<=b[j]){
-            temp[k++] = a[i++];
-        }else {
-            temp[k++] = b[j++];
+    int[] arr ={ 6,4,2,1,7 };
+    mergeSort(arr, 0, arr.length-1);
+    System.out.println(Arrays.toString(arr));;
+}
+static void mergeSort(int[] arr, int left, int right){
+    if (left>=right) return;
+    int mid = left+(right-left)/2;
+    mergeSort(arr, left, mid);
+    mergeSort(arr,mid+1,right);
+    merge(arr, left,mid,right);
+}
+static void merge(int[] arr, int left, int mid, int right){
+    int[] temp = new int[right-left+1];
+    int i = left;
+    int j = mid+1;
+    int k=0;
+
+    while (i<=mid && j<=right){
+        if (arr[i]<=arr[j]){
+            temp[k++] = arr[i++];
+        } else {
+            temp[k++] = arr[j++];
         }
     }
-    while (i<a.length)temp[k++] = a[i++];
-    while (j<b.length)temp[k++] = b[j++];
-
-    return temp;
+    while (i<=mid){
+        temp[k++] = arr[i++];
     }
+    while (j<=right){
+        temp[k++] = arr[j++];
+    }
+    for (int l = 0; l < temp.length; l++) {
+        arr[left+l] = temp[l];
+    }
+}
 }

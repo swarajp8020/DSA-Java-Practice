091d782d5abb361fa1ba7a806d77c3035bfcad7a
SwarajXO <purarkarswaraj@gmail.com>
2025-11-21 08:20:28 +0530

    21-11-25 Practice session-2  quickSortMidPivot (1 peek)

--- JavaPrograms/src/WeekdayPractice/practice21Nov.java
+++ JavaPrograms/src/WeekdayPractice/practice21Nov.java
@@ -5,10 +5,36 @@ import java.util.Arrays;
 public class practice21Nov {
     public static void main(String[] args) {
         int[] arr = {8,6,4,-1,0,23,5};
-        quickSortDNF(arr, 0 ,arr.length-1);
+        quickSortHoare(arr, 0 ,arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 
+//    quickSortMidPivot (1 peek)
+    static void quickSortHoare(int[] arr, int left, int right){
+        if (left>=right)return;
+        int pivotIndex = partitionHoare(arr, left, right);
+        quickSortHoare(arr, left, pivotIndex-1);
+        quickSortHoare(arr, pivotIndex+1, right);
+    }
+    static int partitionHoare(int[] arr, int left, int right){
+
+        int pivot = arr[(left+right)/2];
+        while (true){
+            while (left<right){
+                while (arr[left]<pivot)left++;
+                while (arr[right]>pivot)right--;
+
+                while (left>=right) return right;
+
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+    }
+
+
+    //quicksortDNF (no peek)
     static void quickSortDNF(int[] arr, int left, int right){
         if (left>=right)return;
         int[] bound = partitionDNF(arr, left, right);

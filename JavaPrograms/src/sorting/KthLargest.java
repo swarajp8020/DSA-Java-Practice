c6506d7374b2a6c1f5815a6c23b1ea3de0f18cdb
SwarajXO <purarkarswaraj@gmail.com>
2025-11-23 07:26:00 +0530

    23-11-25 Kth Largest

---
+++ JavaPrograms/src/sorting/KthLargest.java
@@ -0,0 +1,39 @@
+package sorting;
+
+public class KthLargest {
+    public static void main(String[] args) {
+        int[] arr = {8,5,7,2,9,3};
+        int k = 3;
+        int result = quickSelect(arr, 0, arr.length-1, arr.length-k);
+        System.out.println(result);
+    }
+    static int quickSelect(int[] arr, int left, int right, int targetIndex){
+        if (left<=right){
+            int pivotIndex = partition(arr,left,right);
+
+            if (pivotIndex == targetIndex){
+                return arr[pivotIndex];
+            }
+            if (pivotIndex < targetIndex) return quickSelect(arr, pivotIndex+1, right, targetIndex);
+        return quickSelect(arr, left, pivotIndex-1, targetIndex);
+        }
+        return -1;
+    }
+    static int partition(int[] arr, int left, int right){
+        int pivot = arr[right];
+        int i = left-1;
+        for (int j = left; j < right; j++) {
+            if (arr[j]<=pivot){
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i+1, right);
+        return i+1;
+    }
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}

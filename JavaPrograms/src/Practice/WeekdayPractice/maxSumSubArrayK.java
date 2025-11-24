61d9f02b1d3f4f40ac7da05b6d2b1ae00e453ea0
SwarajXO <purarkarswaraj@gmail.com>
2025-11-24 08:18:17 +0530

    24-11-25  Sliding Window MaxSumSubarrayK Practice

--- JavaPrograms/src/Practice/WeekdayPractice/maxSumSubArrayK.java
+++ JavaPrograms/src/Practice/WeekdayPractice/maxSumSubArrayK.java
@@ -6,43 +6,17 @@ public class maxSumSubArrayK {
         int k =3;
         System.out.println(maxSum(arr,k));
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    static int maxSum(int[] arr, int k){
-//        int windowSum = 0;
-//        int maxSum = 0;
-//        for (int i = 0; i < k; i++) {
-//            windowSum += arr[i];
-//        }
-//        maxSum = windowSum;
-//        for (int i = k; i < arr.length; i++) {
-//            windowSum += arr[i] - arr[i-k];
-//            maxSum = Math.max(windowSum, maxSum);
-//        }
-//        return maxSum;
-//    }
+    static int maxSum(int[] arr, int k){
+        int windowSum = 0;
+        int maxSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
+        }
+        maxSum = windowSum;
+        for (int i = k; i < arr.length; i++) {
+            windowSum += arr[i] - arr[i-k];
+            maxSum = Math.max(windowSum, maxSum);
+        }
+        return maxSum;
+    }
 }

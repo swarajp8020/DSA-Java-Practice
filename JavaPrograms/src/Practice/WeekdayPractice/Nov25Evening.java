014847919604e3941fa4573b335361dfbc09b1c1
SwarajXO <purarkarswaraj@gmail.com>
2025-11-25 20:45:04 +0530

    25-11-25  Evening Practice

--- JavaPrograms/src/Practice/WeekdayPractice/Nov25Evening.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Nov25Evening.java
@@ -1,29 +1,95 @@
 package Practice.WeekdayPractice;
 
-/// Minimum subarray sum of size K
-/// Longest subarray sum ≤ X
-/// Longest unique subarray
+import slidingWindows.LongestUniqueSubarray;
+
 public class Nov25Evening {
-    /// Subarrays of size K divisible by 3
+
+/// Longest subarray sum ≤ X
     public static void main(String[] args) {
         int[] arr = {2,1,4,5,2,1};
         int k = 3;
-        int res =countEvenSubArrayK(arr, k);
+        int res = LongestUniqueSubarray(arr, k);
         System.out.println(res);
     }
-    static int countEvenSubArrayK(int[] arr, int k){
-        int count = 0;
-        int windowSum= 0;
-        for (int i = 0; i < k; i++) {
-            windowSum += arr[i];
+    static int LongestUniqueSubarray(int[] arr, int k){
+    int windowSum = 0;
+    int left = 0;
+    int maxLen = 0;
+        for (int right = 0; right < arr.length; right++) {
+            windowSum += arr[right];
+            while (windowSum>k){
+                windowSum -=  arr[left];
+                left++;
+            }
+            maxLen = Math.max(maxLen, right-left+1);
         }
-        if (windowSum%3 == 0) count++;
-        for (int i = k; i < arr.length; i++) {
-            windowSum += arr[i] - arr[i-k];
-            if (windowSum%3==0)count++;
-        }
-        return count;
+        return maxLen;
     }
+
+/// Longest unique subarray
+//public static void main(String[] args) {
+//        int[] arr = {2,1,4,5,2,1};
+//        int k = 3;
+//        int res = LongestUniqueSubarray(arr, k);
+//        System.out.println(res);
+//    }
+//    static int LongestUniqueSubarray(int[] arr, int k){
+//    int[] freq = new int[1001];
+//    int left = 0;
+//    int maxLen = 0;
+//        for (int right = 0; right < arr.length; right++) {
+//            freq[arr[right]]++;
+//            while (freq[arr[right]]>1){
+//                freq[arr[left]]--;
+//                left--;
+//            }
+//            maxLen = Math.max(maxLen, right-left+1);
+//        }
+//        return maxLen;
+//    }
+
+
+    /// Minimum subarray sum of size K
+//    public static void main(String[] args) {
+//        int[] arr = {2,1,4,5,2,1};
+//        int k = 3;
+//        int res =minSumSubArrayK(arr, k);
+//        System.out.println(res);
+//    }
+//    static int minSumSubArrayK(int[] arr, int k){
+//        int windowSum = 0;
+//        int minSum = 0;
+//        for (int i = 0; i < k; i++) {
+//            windowSum += arr[i];
+//        }
+//        minSum = windowSum;
+//        for (int i = k; i < arr.length; i++) {
+//            windowSum += arr[i] - arr[i-k];
+//            minSum = Math.min(windowSum,minSum);
+//        }
+//        return minSum;
+//    }
+
+    /// Subarrays of size K divisible by 3
+//    public static void main(String[] args) {
+//        int[] arr = {2,1,4,5,2,1};
+//        int k = 3;
+//        int res =countEvenSubArrayK(arr, k);
+//        System.out.println(res);
+//    }
+//    static int countEvenSubArrayK(int[] arr, int k){
+//        int count = 0;
+//        int windowSum= 0;
+//        for (int i = 0; i < k; i++) {
+//            windowSum += arr[i];
+//        }
+//        if (windowSum%3 == 0) count++;
+//        for (int i = k; i < arr.length; i++) {
+//            windowSum += arr[i] - arr[i-k];
+//            if (windowSum%3==0)count++;
+//        }
+//        return count;
+//    }
 
 /// Count even sum subarrays of size K
 //public static void main(String[] args) {

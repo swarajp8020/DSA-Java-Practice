b7f038c66922de2fcf9d4ed8616f0ee96d5cb488
SwarajXO <purarkarswaraj@gmail.com>
2025-11-25 14:13:39 +0530

    25-11-25  LongestSubArraySumX Subarrays Practice-1

--- JavaPrograms/src/Practice/WeekdayPractice/Nov25MorningPractice.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Nov25MorningPractice.java
@@ -1,28 +1,31 @@
 package Practice.WeekdayPractice;
 
 public class Nov25MorningPractice {
+
+
+
     ///countEvenSubarrays
-    public static void main(String[] args) {
-        int[] arr = {4,1,2,52,1,2,4};
-        int k = 3;
-        int x= 2;
-        int res = countEvenSubarrays(arr, k ,x);
-        System.out.println(res);
-    }
-    static int countEvenSubarrays(int[] arr, int k, int x){
-        int evenCount = 0;
-        int count = 0;
-        for (int i = 0; i < k; i++) {
-            if (arr[i]%2==0)evenCount++;
-        }
-        if (evenCount>=x) count++;
-        for (int i = k; i < arr.length; i++) {
-            if (arr[i-k]%2==0)evenCount--;
-            if (arr[i]%2==0) evenCount++;
-            if (evenCount>=x)count++;
-        }
-        return count;
-    }
+//    public static void main(String[] args) {
+//        int[] arr = {4,1,2,52,1,2,4};
+//        int k = 3;
+//        int x= 2;
+//        int res = countEvenSubarrays(arr, k ,x);
+//        System.out.println(res);
+//    }
+//    static int countEvenSubarrays(int[] arr, int k, int x){
+//        int evenCount = 0;
+//        int count = 0;
+//        for (int i = 0; i < k; i++) {
+//            if (arr[i]%2==0)evenCount++;
+//        }
+//        if (evenCount>=x) count++;
+//        for (int i = k; i < arr.length; i++) {
+//            if (arr[i-k]%2==0)evenCount--;
+//            if (arr[i]%2==0) evenCount++;
+//            if (evenCount>=x)count++;
+//        }
+//        return count;
+//    }
 
 //    static int countEvenSubarrays(int[] arr, int k, int x){
 //        int countEven= 0;

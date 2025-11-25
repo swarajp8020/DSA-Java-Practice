b220263f357e5c6b668c933a010d49a8a113fad0
SwarajXO <purarkarswaraj@gmail.com>
2025-11-25 07:36:51 +0530

    25-11-25  practice

--- JavaPrograms/src/Practice/WeekdayPractice/Nov25Practice.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Nov25Practice.java
@@ -17,7 +17,6 @@ public class Nov25Practice {
     static int maxSumSubArrayK(int[] arr, int k){
         int windowSum = 0;
         int maxSum = 0;
-        k =0;
         for (int i = 0; i < k; i++) {
             windowSum += arr[i];
         }

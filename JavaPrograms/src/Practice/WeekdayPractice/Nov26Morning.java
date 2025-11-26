ed2ca2849c5209afaa736561064412fba6aa60fd
SwarajXO <purarkarswaraj@gmail.com>
2025-11-26 07:28:19 +0530

    26-11-25  Morning Practice

--- JavaPrograms/src/Practice/WeekdayPractice/Nov26Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Nov26Morning.java
@@ -1,25 +1,49 @@
 package Practice.WeekdayPractice;
 
+import slidingWindows.LongestSubArraySumX;
+
 public class Nov26Morning {
-//    Longest Unique Subarray (no peek)
-    public static void main(String[] args) {
-        int[] arr = {4,2,1,5,1,51,2};
-        int k = 3;
-        int res = longestUniqueSubarray(arr, k);
-        System.out.println(res);
-    }
-    static int longestUniqueSubarray(int[] arr, int k){
-        int[] freq = new int[1001];
-        int left = 0;
-        int maxLen = 0;
-        for (int right = 0; right < k; right++) {
-            freq[arr[right]]++;
-            while (freq[arr[right]]>1){
-                freq[arr[left]]--;
-                left++;
-            }
-            maxLen = Math.max(maxLen, right-left+1);
+//    Longest Subarray Sum ≤ X
+public static void main(String[] args) {
+    int[] arr = {4,2,1,5,1,51,2};
+    int x = 7;
+    int res = LongestSubArraySumX(arr, x);
+    System.out.println(res);
+}
+static int LongestSubArraySumX(int[] arr, int x){
+    int windowSum = 0;
+    int left = 0;
+    int maxLen = 0;
+    for (int right = 0; right < arr.length; right++) {
+        windowSum += arr[right];
+        while (windowSum>x){
+            windowSum -= arr[left];
+            left++;
         }
-        return maxLen;
+        maxLen = Math.max(maxLen, right-left+1);
     }
+    return maxLen;
+}
+
+//    Longest Unique Subarray (no peek)
+//    public static void main(String[] args) {
+//        int[] arr = {4,2,1,5,1,51,2};
+//        int k = 3;
+//        int res = longestUniqueSubarray(arr, k);
+//        System.out.println(res);
+//    }
+//    static int longestUniqueSubarray(int[] arr, int k){
+//        int[] freq = new int[1001];
+//        int left = 0;
+//        int maxLen = 0;
+//        for (int right = 0; right < k; right++) {
+//            freq[arr[right]]++;
+//            while (freq[arr[right]]>1){
+//                freq[arr[left]]--;
+//                left++;
+//            }
+//            maxLen = Math.max(maxLen, right-left+1);
+//        }
+//        return maxLen;
+//    }
 }

32ca46c917d1bf066b9ed5371cf11cb244575cdf
SwarajXO <purarkarswaraj@gmail.com>
2025-11-28 08:16:48 +0530

    28-11-25  Morning Practice

--- JavaPrograms/src/Practice/WeekdayPractice/Nov28Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Nov28Morning.java
@@ -1,35 +1,88 @@
 package Practice.WeekdayPractice;
 /// PHASE B (Prime muscle 30–40 mins)
 public class Nov28Morning {
-
-
-    /// ✔ Longest Subarray with Exactly K Distinct  ← new checkpoint [3 times peek]
-    public static void main(String[] args) {
-        int[] arr = {3,1,2,5,4,6};
-        int k =3;
-        System.out.println(exactlyKDistinct(arr,k));
-    }
-    static int exactlyKDistinct(int[] arr, int k){
-        int[] freq = new int[1001];
-        int unique = 0;
+/// Longest Subarray Sum ≤ X (no peek)
+public static void main(String[] args) {
+    int[] arr= {1,2,3,4,5,6,7};
+    int x= 3;
+    System.out.println(longestUniqueSubarrayX(arr,x));
+}
+    static int longestUniqueSubarrayX(int[] arr, int x){
+        int windowSum = 0;
         int left = 0;
         int maxLen = 0;
         for (int right = 0; right < arr.length; right++) {
-            if (freq[arr[right]]==0) unique++;
-            freq[arr[right]]++;
-
-            while (unique>k){
-                freq[arr[left]]--;
-                if (freq[arr[left]]==0)unique--;
+            windowSum += arr[right];
+            while (windowSum>x){
+                windowSum -= arr[left];
                 left++;
-            }
-            if (unique==k) {
-                maxLen = Math.max(maxLen, right - left + 1);
-            }
+        }
+            maxLen = Math.max(maxLen, right-left+1);
         }
         return maxLen;
-
     }
+
+/// Longest Unique Subarray (no peek)
+//public static void main(String[] args) {
+//    int[] arr= {1,2,3,4,5,6,7};
+//    System.out.println(longestUniqueSubarray(arr));
+//}
+//static int longestUniqueSubarray(int[] arr){
+//    int[] freq = new int[1001];
+//    int left = 0;
+//    int maxLen = 0;
+//    for (int right = 0; right < arr.length; right++) {
+//        freq[arr[right]]++;
+//        while (freq[arr[right]]>1){
+//            freq[arr[left]]--;
+//            left++;
+//        }
+//        maxLen = Math.max(maxLen, right-left+1);
+//    }
+//    return maxLen;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    /// ✔ Longest Subarray with Exactly K Distinct  ← new checkpoint [3 times peek]
+//    public static void main(String[] args) {
+//        int[] arr = {3,1,2,5,4,6};
+//        int k =3;
+//        System.out.println(exactlyKDistinct(arr,k));
+//    }
+//    static int exactlyKDistinct(int[] arr, int k){
+//        int[] freq = new int[1001];
+//        int unique = 0;
+//        int left = 0;
+//        int maxLen = 0;
+//        for (int right = 0; right < arr.length; right++) {
+//            if (freq[arr[right]]==0) unique++;
+//            freq[arr[right]]++;
+//
+//            while (unique>k){
+//                freq[arr[left]]--;
+//                if (freq[arr[left]]==0)unique--;
+//                left++;
+//            }
+//            if (unique==k) {
+//                maxLen = Math.max(maxLen, right - left + 1);
+//            }
+//        }
+//        return maxLen;
+//
+//    }
 
 /// ✔ Longest Unique Subarray
 //public static void main(String[] args) {

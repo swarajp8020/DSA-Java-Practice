b7f337c82c19efb1411173db768d4b533e8374e5
SwarajXO <purarkarswaraj@gmail.com>
2025-11-28 06:42:51 +0530

    28-11-25  Morning Practice Phase-B

--- JavaPrograms/src/Practice/WeekdayPractice/Nov28Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Nov28Morning.java
@@ -1,12 +1,104 @@
 package Practice.WeekdayPractice;
-/// PHASE A (Warm-up 20–30 mins)
-///
 /// PHASE B (Prime muscle 30–40 mins)
-/// ✔ Longest Sum ≤ X
-/// ✔ Longest Sum = X
-/// ✔ Longest Unique Subarray
-/// ✔ Longest Subarray with Exactly K Distinct  ← new checkpoint
 public class Nov28Morning {
+
+
+    /// ✔ Longest Subarray with Exactly K Distinct  ← new checkpoint [3 times peek]
+    public static void main(String[] args) {
+        int[] arr = {3,1,2,5,4,6};
+        int k =3;
+        System.out.println(longestUniqueSubarray(arr,k));
+    }
+    static int longestUniqueSubarray(int[] arr, int k){
+        int[] freq = new int[1001];
+        int unique = 0;
+        int left = 0;
+        int maxLen = 0;
+        for (int right = 0; right < arr.length; right++) {
+            if (freq[arr[right]]==0)unique++;
+            freq[arr[right]]++;
+            while (unique>k){
+                freq[arr[left]]--;
+                if (freq[arr[left]]==0)unique--;
+                left++;
+            } if (unique==k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+        return maxLen;
+
+    }
+
+/// ✔ Longest Unique Subarray
+//public static void main(String[] args) {
+//    int[] arr = {3,1,2,5,4,6};
+//    System.out.println(longestUniqueSubarray(arr));
+//}
+//static int longestUniqueSubarray(int[] arr){
+//    int[] freq = new int[1001];
+//    int left = 0;
+//    int maxLem = 0;
+//    for (int right = 0; right < arr.length; right++) {
+//        freq[arr[right]]++;
+//        while (freq[arr[right]]>1){
+//            freq[arr[left]]--;
+//            left++;
+//        }
+//        maxLem = Math.max(maxLem, right-left+1);
+//    }
+//    return maxLem;
+//}
+
+
+/// ✔ Longest Sum = X
+//public static void main(String[] args) {
+//    int[] arr = {3,1,2,5,4,6};
+//    int x = 5;
+//    int res = longestSumX(arr,x);
+//    System.out.println(res);
+//}
+//static int longestSumX(int[] arr, int x){
+//    int windowSum= 0;
+//    int left=0;
+//    int maxLen = 0;
+//    for (int right = 0; right < arr.length; right++) {
+//        windowSum += arr[right];
+//        while (windowSum>x){
+//            windowSum -= arr[left];
+//            left++;
+//        }
+//        if (windowSum==x){
+//            maxLen = Math.max(maxLen, right-left+1);
+//        }
+//    }
+//    return maxLen;
+//}
+
+
+/// ✔ Longest Sum ≤ X
+//public static void main(String[] args) {
+//    int[] arr = {3,1,2,5,4,6};
+//    int x = 5;
+//    int res = longestSumX(arr,x);
+//    System.out.println(res);
+//}
+//static int longestSumX(int[] arr, int x){
+//    int windowSum= 0;
+//    int left=0;
+//    int maxLen = 0;
+//    for (int right = 0; right < arr.length; right++) {
+//        windowSum += arr[right];
+//        while (windowSum>x){
+//            windowSum -= arr[left];
+//            left++;
+//        }
+//        maxLen = Math.max(maxLen, right-left+1);
+//    }
+//    return maxLen;
+//}
+
+
+
     /// ✔ Max Sum K
 //    public static void main(String[] args) {
 //        int[] arr = {3,5,2,1,4,6};
@@ -49,23 +141,23 @@ public class Nov28Morning {
 //    }
 
     /// ✔ Count K divisible by 2 or 3
-public static void main(String[] args) {
-    int[] arr = {3,5,2,1,4,6};
-    int k = 3;
-    int res = countSubarrayDivK(arr, k);
-    System.out.println(res);
-}
-    static int countSubarrayDivK(int[] arr, int k) {
-        int count = 0;
-        int windowSum = 0;
-        for (int i = 0; i < k; i++) {
-            windowSum += arr[i];
-        }
-        if (windowSum%2==0) count++;
-        for (int i = k; i < arr.length; i++) {
-            windowSum += arr[i]-arr[i-k];
-            if (windowSum%2==0)count++;
-        }
-        return count;
-    }
+//public static void main(String[] args) {
+//    int[] arr = {3,5,2,1,4,6};
+//    int k = 3;
+//    int res = countSubarrayDivK(arr, k);
+//    System.out.println(res);
+//}
+//    static int countSubarrayDivK(int[] arr, int k) {
+//        int count = 0;
+//        int windowSum = 0;
+//        for (int i = 0; i < k; i++) {
+//            windowSum += arr[i];
+//        }
+//        if (windowSum%2==0) count++;
+//        for (int i = k; i < arr.length; i++) {
+//            windowSum += arr[i]-arr[i-k];
+//            if (windowSum%2==0)count++;
+//        }
+//        return count;
+//    }
 }

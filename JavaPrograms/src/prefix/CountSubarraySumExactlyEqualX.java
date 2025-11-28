99d3a2de9ff991fbfb690fb254aab3e707a995ff
SwarajXO <purarkarswaraj@gmail.com>
2025-11-28 13:10:13 +0530

    28-11-25  countSubarraySumX

---
+++ JavaPrograms/src/prefix/CountSubarraySumExactlyEqualX.java
@@ -0,0 +1,22 @@
+package prefix;
+
+public class CountSubarraySumExactlyEqualX {
+    public static void main(String[] args) {
+        int[] arr = {4,1,2,1,1,3};
+        int x = 5;
+        System.out.println(countSubarraySumX(arr,x));
+    }
+    static int countSubarraySumX(int[] arr, int x){
+        int count = 0;
+        for (int start = 0; start < arr.length; start++) {
+            int sum = 0;
+            for (int end = 0; end < arr.length; end++) {
+                sum += arr[end];
+                if (sum==x){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}

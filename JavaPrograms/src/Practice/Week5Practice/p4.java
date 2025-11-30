140c90dc874a5b0d9b6458ed96511b12f3fedf75
SwarajXO <purarkarswaraj@gmail.com>
2025-11-30 09:18:48 +0530

    30-11-25  Binary Search All 3 pattern Practice

--- JavaPrograms/src/Practice/Week5Practice/p4.java
+++ JavaPrograms/src/Practice/Week5Practice/p4.java
@@ -25,13 +25,11 @@ public class p4 {
     }
 
 /// Pattern 2 — First/Last Occurrence
-//    arr = [2,5,7,9,12], target=9 → output = 3
+//    first([1,2,2,2,3],2)
 //public static void main(String[] args) {
-//    int[] arr = {2,5,7,9,12};
-//    int target= 9;
-//    System.out.println(search(arr, target));
+//    int[] arr = {1,2,2,2,3};
+//    int target= 2;
 //    System.out.println(first(arr, target));
-//    System.out.println(last(arr, target));
 //}
 //    static int first(int[] arr, int target){
 //        int left =0,right=arr.length-1, ans =-1;
@@ -54,18 +52,22 @@ public class p4 {
 //             ans = mid;
 //             left = mid+1;
 //            }
-//            if (arr[mid]<target) left = mid+1;
+//            else if (arr[mid]<target) left = mid+1;
 //            else right = mid-1;
 //        }
 //        return -1;
 //    }
-//
+//public static void main(String[] args) {
+////    int[] arr = {1,2,2,2,3};
+////    int target= 2;
+////    System.out.println(last(arr, target));
+////}
 //static int search(int[] arr, int target){
 //    int left =0,right=arr.length-1;
 //    while (left<=right){
 //        int mid = left+(right-left)/2;
 //        if (arr[mid]==target) return mid;
-//        if (arr[mid]<target) left = mid+1;
+//        else if (arr[mid]<target) left = mid+1;
 //        else right = mid-1;
 //    }
 //    return -1;
@@ -74,8 +76,8 @@ public class p4 {
     /// pattern-1 Binary Search
     /// arr = [2,5,7,9,12], target=9 → output = 3
 //    public static void main(String[] args) {
-//        int[] arr = {2,5,7,9,12};
-//        int target = 9;
+//        int[] arr = {2,4,6,8,10};
+//        int target = 6;
 //        System.out.println(search(arr,target));
 //    }
 //    static int search(int[] arr, int target){

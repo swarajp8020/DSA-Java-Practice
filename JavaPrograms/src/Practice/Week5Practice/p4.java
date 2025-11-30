1e76313a205e2c7fa564dbcb5a4505258b816dfc
SwarajXO <purarkarswaraj@gmail.com>
2025-11-30 07:17:12 +0530

    30-11-25  Binary Search Pattern 1 — Find element / normal BS Practice

---
+++ JavaPrograms/src/Practice/Week5Practice/p4.java
@@ -0,0 +1,21 @@
+package Practice.Week5Practice;
+
+public class p4 {
+    /// pattern-1 Binary Search
+    /// arr = [2,5,7,9,12], target=9 → output = 3
+    public static void main(String[] args) {
+        int[] arr = {2,5,7,9,12};
+        int target = 9;
+        System.out.println(search(arr,target));
+    }
+    static int search(int[] arr, int target){
+        int left=0, right =arr.length-1;
+        while (left<=right){
+            int mid = left+(right-left)/2;
+            if (arr[mid]==target)return mid;
+            if (arr[mid]<target)left=mid+1;
+            else right=mid-1;
+        }
+        return -1;
+    }
+}

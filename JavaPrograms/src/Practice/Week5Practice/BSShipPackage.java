4c24f54c0ad8c7d8c777320c8cb70f134fe98bde
SwarajXO <purarkarswaraj@gmail.com>
2025-12-01 07:26:13 +0530

    01-12-25  Binary Search Ship package Leetcode practice

--- JavaPrograms/src/Practice/Week5Practice/BSShipPackage.java
+++ JavaPrograms/src/Practice/Week5Practice/BSShipPackage.java
@@ -10,39 +10,33 @@ public class BSShipPackage {
         int low = max(arr);
         int high = sum(arr);
         int ans = high;
-
         while (low<=high){
             int mid = low+(high-low)/2;
-
-            if (canShip(arr, D, mid)){
-                ans = mid;
-                high = mid-1;
-            }else {
-                low =mid+1;
-            }
+            if (canShip(arr,D,mid)){
+                ans=mid;
+                high=mid-1;
+            }else low=mid+1;
         }
         return ans;
     }
-    static boolean canShip(int[]arr,int D, int cap){
-        int days = 1 , load = 0;
-
-        for(int w:arr){
-            if(load + w > cap){
+    static boolean canShip(int[] arr, int D, int can){
+        int days =1, load =0;
+        for (int x: arr){
+            if (load+x>can){
                 days++;
-                load = w;
-            }else load += w;
+                load=x;
+            }else load+=x;
         }
-        return days <= D;
+        return days <=D;
     }
     static int max(int[]arr){
         int m = 0;
-        for (int x:arr)m=Math.max(m,x);
+        for (int y:arr)m=Math.max(m,y);
         return m;
     }
     static int sum(int[]arr){
-        int s =0;
-        for (int y:arr)s+=y;
+        int s=0;
+        for (int z:arr)s+=z;
         return s;
     }
-
 }

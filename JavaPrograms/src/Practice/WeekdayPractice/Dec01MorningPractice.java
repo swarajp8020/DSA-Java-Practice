6985778effd8536310c7d48e9c9e5977adc5c628
SwarajXO <purarkarswaraj@gmail.com>
2025-12-01 07:56:57 +0530

    01-12-25  Binary Search LC 875 — Koko Eating Bananas practice

---
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec01MorningPractice.java
@@ -0,0 +1,36 @@
+package Practice.WeekdayPractice;
+
+public class Dec01MorningPractice {
+    public static void main(String[] args) {
+        int[] piles = {3,6,7,11};
+        int H = 8;
+        System.out.println(minEatingSpeed(piles,H));
+    }
+    static int minEatingSpeed(int[]piles,int H){
+        int low = 1;
+        int high = max(piles);
+        int ans = high;
+        while (low<=high){
+            int speed = low+(high-low)/2;
+            if (canEat(piles,H,speed)){
+                ans=speed;
+                high=speed-1;
+            }else low=speed+1;
+        }
+        return ans;
+    }
+    static boolean canEat(int[]piles, int H, int speed){
+        int hoursUsed = 0;
+        for (int pile:piles){
+            hoursUsed+=(pile+speed-1)/speed;
+            if (hoursUsed>H)return false;
+        }
+        return true;
+    }
+
+    static int max(int[]arr){
+        int m = 0;
+        for (int x:arr)m=Math.max(m,x);
+        return m;
+    }
+}

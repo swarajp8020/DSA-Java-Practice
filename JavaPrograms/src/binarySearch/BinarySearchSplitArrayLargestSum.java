ba2bd8a66d9d0fa39a15b1bd223cb892cb8b221a
SwarajXO <purarkarswaraj@gmail.com>
2025-12-01 09:33:15 +0530

    01-12-25  Split Array Largest Sum — LeetCode 410

---
+++ JavaPrograms/src/binarySearch/BinarySearchSplitArrayLargestSum.java
@@ -0,0 +1,44 @@
+package binarySearch;
+/// Split Array Largest Sum — LeetCode 410
+public class BinarySearchSplitArrayLargestSum {
+    public static void main(String[] args) {
+        int[] nums = {7,2,5,10,8};
+        int k = 2;
+        System.out.println(splitArray(nums, k)); // expected 14
+    }
+    static int splitArray(int[] nums, int k){
+        int low = max(nums);
+        int high = sum(nums);
+        int ans = high;
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if (canSplit(nums,k,mid)){
+                ans=mid;
+                high=mid-1;
+            }else low=mid+1;
+        }
+        return ans;
+    }
+    static boolean canSplit(int[] arr, int k, int limit){
+        int groupCount = 0, currentSum=0;
+        for (int num: arr){
+            if (currentSum+num>limit){
+                groupCount++;
+                currentSum=num;
+                if (groupCount>k)return false;
+            } else currentSum+= num;
+        }
+        return true;
+    }
+
+    static int max(int[]arr){
+        int m = 0;
+        for (int x:arr)m=Math.max(m,x);
+        return m;
+    }
+    static int sum(int[]arr){
+        int s=0;
+        for (int x:arr)s+=x;
+        return s;
+    }
+}

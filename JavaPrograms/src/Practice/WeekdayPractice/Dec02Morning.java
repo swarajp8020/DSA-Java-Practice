3171344b4ba9d34741481c2875ee38e34130b53c
SwarajXO <purarkarswaraj@gmail.com>
2025-12-02 06:41:24 +0530

    02-12-25  LC 410 — Split Array Largest Sum without peeking solution

--- JavaPrograms/src/Practice/WeekdayPractice/Dec02Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec02Morning.java
@@ -2,35 +2,84 @@ package Practice.WeekdayPractice;
 
 public class Dec02Morning {
     public static void main(String[] args) {
-        int[] piles = {30,11,23,4,20};
-        int h = 5;
-        System.out.println(minEatingSpeed(piles,h));
+        int[] nums = {7,2,5,10,8};
+        int k =2;
+        System.out.println(splitArray(nums, k));
+        System.out.println(splitArray(new int[]{7,2,5,10,8},2));   // 18
+        System.out.println(splitArray(new int[]{1,4,4},3));       // 4
+        System.out.println(splitArray(new int[]{1,2,3,4,5},2));   // 9
+        System.out.println(splitArray(new int[]{3,6,7,11},1));    // 27
+
     }
-    static int minEatingSpeed(int[] piles, int h){
-        int low =1;
-        int high = max(piles);
-        int ans=high;
+    static int splitArray(int[] nums, int k){
+        int low = max(nums);
+        int high = sum(nums);
+        int ans = high;
         while (low<=high){
-            int speed = low+(high-low)/2;
-            if (canEat(piles, h, speed)){
-                ans = speed;
-                high = speed-1;
-            } else low = speed+1;
+            int mid = low+(high-low)/2;
+            if (canSplit(nums,k,mid)){
+                ans = mid;
+                high = mid-1;
+
+            } else low=mid+1;
+
         }
         return ans;
     }
-    static boolean canEat(int[] piles, int h, int speed){
-        int hoursUsed = 0;
-        for (int pile:piles){
-            hoursUsed += (pile+speed-1)/speed;
-            if (hoursUsed>h) return false;
-        } return true;
+    static boolean canSplit(int[]nums, int k, int limit){
+        int groupCount =1, currSum = 0;
+        for (int x: nums){
+            if (currSum+x>limit){
+                groupCount++;
+                currSum=x;
+                if (groupCount>k) return false;
+            } else currSum += x;
+        }
+        return groupCount <= k;
     }
     static int max(int[] arr){
         int m = 0;
-        for (int x:arr)m=Math.max(m,x);
+        for (int x: arr)m=Math.max(m,x);
         return m;
     }
+    static int sum(int[]arr){
+        int s=0;
+        for (int x:arr)s+=x;
+        return s;
+    }
+
+
+
+//    public static void main(String[] args) {
+//        int[] piles = {30,11,23,4,20};
+//        int h = 5;
+//        System.out.println(minEatingSpeed(piles,h));
+//    }
+//    static int minEatingSpeed(int[] piles, int h){
+//        int low =1;
+//        int high = max(piles);
+//        int ans=high;
+//        while (low<=high){
+//            int speed = low+(high-low)/2;
+//            if (canEat(piles, h, speed)){
+//                ans = speed;
+//                high = speed-1;
+//            } else low = speed+1;
+//        }
+//        return ans;
+//    }
+//    static boolean canEat(int[] piles, int h, int speed){
+//        int hoursUsed = 0;
+//        for (int pile:piles){
+//            hoursUsed += (pile+speed-1)/speed;
+//            if (hoursUsed>h) return false;
+//        } return true;
+//    }
+//    static int max(int[] arr){
+//        int m = 0;
+//        for (int x:arr)m=Math.max(m,x);
+//        return m;
+//    }
 
 
 

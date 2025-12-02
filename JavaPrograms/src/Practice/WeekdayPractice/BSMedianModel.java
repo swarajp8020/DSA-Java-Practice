01844068a6f4fa96decdbd3b850d24ba7823f706
SwarajXO <purarkarswaraj@gmail.com>
2025-12-02 13:17:07 +0530

    02-12-25  LC 4 Median of Two Sorted Arrays Practice test case-2

--- JavaPrograms/src/Practice/WeekdayPractice/BSMedianModel.java
+++ JavaPrograms/src/Practice/WeekdayPractice/BSMedianModel.java
@@ -2,32 +2,32 @@ package Practice.WeekdayPractice;
 
 public class BSMedianModel {
     public static void main(String[] args) {
-        int[] A = {1,2};
-        int[] B = {3,4};
+        int[] A = {1,3};
+        int[] B = {2};
         System.out.println(findMedianSortedArrays(A,B));
     }
     static double findMedianSortedArrays(int[] A, int[] B){
-        if (A.length>B.length)return findMedianSortedArrays(B,A);
+        if (A.length>B.length) return findMedianSortedArrays(B,A);
         int n = A.length;
         int m = B.length;
         int low = 0, high = n;
         while (low<=high){
-            int cutA = (high+low)/2;
+            int cutA = (low+high)/2;
             int cutB = (n+m+1)/2-cutA;
 
             int Aleft = (cutA==0)?Integer.MIN_VALUE:A[cutA-1];
-            int Bright = (cutA==n)?Integer.MAX_VALUE:A[cutA];
+            int Aright = (cutA==n)?Integer.MAX_VALUE:A[cutA];
 
             int Bleft = (cutB==0)?Integer.MIN_VALUE:B[cutB-1];
-            int Aright = (cutB==m)?Integer.MAX_VALUE:B[cutB];
+            int Bright = (cutB==m)?Integer.MAX_VALUE:B[cutB];
 
-            if (Aleft <= Bright && Bleft <= Aright){
+            if (Aleft<=Bright && Bleft<=Aright){
                 if ((n+m)%2==0){
-                    return (Math.max(Aleft, Bright)+Math.min(Bleft,Aright))/2;
+                    return (Math.max(Aleft,Bleft)+Math.min(Aright,Bright))/2.0;
                 }
                 return Math.max(Aleft,Bleft);
-            } else if (Aleft>Bleft) {
-                high = cutA -1;
+            } else if (Aleft>Bright) {
+                high = cutA-1;
             } else {
                 low = cutA +1;
             }

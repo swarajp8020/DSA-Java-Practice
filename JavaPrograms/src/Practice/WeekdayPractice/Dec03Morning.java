7a226ea87790ad9990ddb3dd1ed64dc58989c10a
SwarajXO <purarkarswaraj@gmail.com>
2025-12-03 06:49:36 +0530

    03-12-25   4. Median of Two Sorted Arrays

--- JavaPrograms/src/Practice/WeekdayPractice/Dec03Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec03Morning.java
@@ -1,48 +1,78 @@
 package Practice.WeekdayPractice;
 
 public class Dec03Morning {
-    /// 410. Split Array Largest Sum
+    /// 4. Median of Two Sorted Arrays
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5};
-        int k = 2;
-        System.out.println(splitArray(nums, k));
+        int[] A = {1,2};
+        int[] B = {3,4};
+        System.out.println(medianSortedArray(A,B));
     }
-
-    static int splitArray(int[] nums, int k){
-        int low = max(nums);
-        int high = sum(nums);
-        int ans = high;
+    static double medianSortedArray(int[]A, int[] B){
+        if (A.length>B.length)return medianSortedArray(B,A);
+        int n = A.length;
+        int m = B.length;
+        int low = 0, high =n;
         while (low<=high){
-            int mid = low+(high-low)/2;
-            if (canSplit(nums,k,mid)){
-                ans = mid;
-                high = mid-1;
-            } else low = mid+1;
-        }
-        return ans;
-    }
-    static boolean canSplit(int[]nums,int k, int limit){
-        int currentSum = 0, groupCount=1;
-        for (int a:nums){
-            if (currentSum+a>limit){
-                groupCount++;
-                currentSum = a;
-                if (groupCount>k) return false;
-            } else currentSum += a;
-        }
-        return groupCount >= k;
-    }
+            int cutA = (low+high)/2;
+            int cutB = (n+m+1)/2-cutA;
 
-    static int max(int[] arr){
-        int m = 0;
-        for (int x:arr)m=Math.max(m,x);
-        return m;
+            int Aleft = (cutA==0)?Integer.MIN_VALUE:A[cutA-1];
+            int Aright = (cutA==n)?Integer.MAX_VALUE:A[cutA];
+            int Bleft = (cutB==0)?Integer.MIN_VALUE:B[cutB-1];
+            int Bright = (cutB==m)?Integer.MAX_VALUE:B[cutB];
+            if (Aleft <= Bright && Bleft <=Aright){
+                if ((n+m)%2==0){
+                    return (Math.max(Aleft,Bleft)+Math.min(Aright, Bright))/2.0;
+                }
+                return Math.max(Aleft,Bleft);
+            } else if (Aleft>Bright) {
+                high = cutA-1;
+            } else low = cutA+1;
+        }
+        return 0;
     }
-    static int sum(int[] arr){
-        int s = 0;
-        for (int x:arr)s+=x;
-        return s;
-    }
+    /// 410. Split Array Largest Sum
+//    public static void main(String[] args) {
+//        int[] nums = {1,2,3,4,5};
+//        int k = 2;
+//        System.out.println(splitArray(nums, k));
+//    }
+//
+//    static int splitArray(int[] nums, int k){
+//        int low = max(nums);
+//        int high = sum(nums);
+//        int ans = high;
+//        while (low<=high){
+//            int mid = low+(high-low)/2;
+//            if (canSplit(nums,k,mid)){
+//                ans = mid;
+//                high = mid-1;
+//            } else low = mid+1;
+//        }
+//        return ans;
+//    }
+//    static boolean canSplit(int[]nums,int k, int limit){
+//        int currentSum = 0, groupCount=1;
+//        for (int a:nums){
+//            if (currentSum+a>limit){
+//                groupCount++;
+//                currentSum = a;
+//                if (groupCount>k) return false;
+//            } else currentSum += a;
+//        }
+//        return groupCount >= k;
+//    }
+//
+//    static int max(int[] arr){
+//        int m = 0;
+//        for (int x:arr)m=Math.max(m,x);
+//        return m;
+//    }
+//    static int sum(int[] arr){
+//        int s = 0;
+//        for (int x:arr)s+=x;
+//        return s;
+//    }
 
 /// 875. Koko Eating Bananas
 //    public static void main(String[] args) {

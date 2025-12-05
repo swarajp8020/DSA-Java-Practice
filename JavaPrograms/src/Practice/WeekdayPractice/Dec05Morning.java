50db5706f887d3ee06be827fb330f322e3ab7edf
SwarajXO <purarkarswaraj@gmail.com>
2025-12-05 06:27:07 +0530

    05-12-25   410. Split Array Largest Sum Test Case-2

--- JavaPrograms/src/Practice/WeekdayPractice/Dec05Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec05Morning.java
@@ -4,7 +4,7 @@ public class Dec05Morning {
 
     /// 410. Split Array Largest Sum
     public static void main(String[] args) {
-        int[] nums = {7,2,5,10,8};
+        int[] nums = {1,2,3,4,5};
         int k = 2;
         System.out.println(splitArray(nums, k));
     }

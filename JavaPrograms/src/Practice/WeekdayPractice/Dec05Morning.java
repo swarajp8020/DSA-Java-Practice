cce87f50acbac3cb4eab14115a079a58f7361307
SwarajXO <purarkarswaraj@gmail.com>
2025-12-05 06:26:40 +0530

    05-12-25   410. Split Array Largest Sum Test Case-1

--- JavaPrograms/src/Practice/WeekdayPractice/Dec05Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec05Morning.java
@@ -2,39 +2,79 @@ package Practice.WeekdayPractice;
 
 public class Dec05Morning {
 
-    /// 875. Koko Eating Bananas
+    /// 410. Split Array Largest Sum
     public static void main(String[] args) {
-        int[] piles = {30,11,23,4,20};
-        int h = 6;
-        System.out.println(eatingBanana(piles, h));
+        int[] nums = {7,2,5,10,8};
+        int k = 2;
+        System.out.println(splitArray(nums, k));
     }
-    static int eatingBanana(int[] piles, int h){
-        int low =0;
-        int high = max(piles);
+    static int splitArray(int[] nums, int k){
+        int low = max(nums);
+        int high = sum(nums);
         int ans = high;
         while (low<=high){
-            int speed = low+(high-low)/2;
-            if (canEat(piles, h, speed)){
-                ans = speed;
-                high = speed-1;
-            } else low = speed +1;
+            int mid = low+(high-low)/2;
+            if (canSplit(nums,k,mid)){
+                ans = mid;
+                high = mid-1;
+            } else low = mid+1;
         }
         return ans;
     }
-    static boolean canEat(int[] piles, int h, int speed){
-        int  hoursUsed = 0;
-        for (int x: piles){
-            hoursUsed += (x+speed-1)/speed;
-            if (hoursUsed>h)return false;
+    static boolean canSplit(int[]nums, int k, int limit){
+        int groupCount = 1, currentSum = 0;
+        for (int x:nums){
+            if (currentSum+x>limit){
+                groupCount++;
+                currentSum = x;
+                if (groupCount>k) return false;
+            } else currentSum += x;
         }
-        return true;
+        return limit >= groupCount;
     }
-
     static int max(int[] arr){
         int m=0;
         for (int x:arr)m=Math.max(m,x);
         return m;
     }
+    static int sum(int[] arr){
+        int s=0;
+        for (int x:arr)s+=x;
+        return s;
+    }
+    /// 875. Koko Eating Bananas
+//    public static void main(String[] args) {
+//        int[] piles = {30,11,23,4,20};
+//        int h = 6;
+//        System.out.println(eatingBanana(piles, h));
+//    }
+//    static int eatingBanana(int[] piles, int h){
+//        int low =0;
+//        int high = max(piles);
+//        int ans = high;
+//        while (low<=high){
+//            int speed = low+(high-low)/2;
+//            if (canEat(piles, h, speed)){
+//                ans = speed;
+//                high = speed-1;
+//            } else low = speed +1;
+//        }
+//        return ans;
+//    }
+//    static boolean canEat(int[] piles, int h, int speed){
+//        int  hoursUsed = 0;
+//        for (int x: piles){
+//            hoursUsed += (x+speed-1)/speed;
+//            if (hoursUsed>h)return false;
+//        }
+//        return true;
+//    }
+//
+//    static int max(int[] arr){
+//        int m=0;
+//        for (int x:arr)m=Math.max(m,x);
+//        return m;
+//    }
 ///    1011. Capacity To Ship Packages Within D Days
 //    public static void main(String[] args) {
 //        int[] weights = {1,2,3,1,1};

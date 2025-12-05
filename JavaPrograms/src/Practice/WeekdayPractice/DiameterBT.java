c04acffe18b0a4acb25befb9edd84ebb29f320fe
SwarajXO <purarkarswaraj@gmail.com>
2025-12-05 09:18:38 +0530

    05-12-25  diameterOfBinaryTree practice

---
+++ JavaPrograms/src/Practice/WeekdayPractice/DiameterBT.java
@@ -0,0 +1,31 @@
+package Practice.WeekdayPractice;
+class TreeNode1{
+    int value;
+    TreeNode1 left,right;
+    TreeNode1(int x){value =x;}
+}
+public class DiameterBT {
+    static int diameter = 0;
+
+    public static int diameterOfBT(TreeNode1 node){
+        height(node);
+        return diameter;
+    }
+    private static int height(TreeNode1 root){
+        if (root==null)return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+
+        diameter = Math.max(diameter, left+right);
+        return 1+Math.max(left,right);
+    }
+
+    public static void main(String[] args) {
+        TreeNode1 node1 = new TreeNode1(1);
+        node1.left = new TreeNode1(2);
+        node1.right = new TreeNode1(3);
+        node1.left.left = new TreeNode1(4);
+        node1.left.right = new TreeNode1(5);
+        System.out.println(diameterOfBT(node1));
+    }
+}

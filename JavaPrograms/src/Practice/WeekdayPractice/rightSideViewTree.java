f5f1da08085912acb1359a1c8c15081669d84dc0
SwarajXO <purarkarswaraj@gmail.com>
2025-12-08 08:23:35 +0530

    08-12-25  rightSideViewTree practice

--- JavaPrograms/src/Practice/WeekdayPractice/rightSideViewTree.java
+++ JavaPrograms/src/Practice/WeekdayPractice/rightSideViewTree.java
@@ -8,16 +8,16 @@ import java.util.Queue;
 class Solution22{
     public List<Integer> rightSideView(TreeNode root){
         List<Integer> result = new ArrayList<>();
-        if (root==null) return result;
-        Queue<TreeNode> q = new LinkedList<>();
+        if (root==null)return result;
+        Queue<TreeNode> q =new LinkedList<>();
         q.add(root);
         while (!q.isEmpty()) {
             int size = q.size();
             TreeNode last = null;
             for (int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
-                last =node;
-                if (node.left!=null)q.add(node.left);
+                last = node;
+                if (node.left!= null)q.add(node.left);
                 if (node.right!=null)q.add(node.right);
             }
             result.add(last.val);

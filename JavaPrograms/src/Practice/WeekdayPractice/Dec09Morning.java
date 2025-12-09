73f8994068e3c68a856a0671b4fcc0f6f6800200
SwarajXO <purarkarswaraj@gmail.com>
2025-12-09 05:43:44 +0530

    09-12-25  Practice 875. Koko Eating Bananas Test-2

--- JavaPrograms/src/Practice/WeekdayPractice/Dec09Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec09Morning.java
@@ -4,8 +4,8 @@ public class Dec09Morning {
     /// 875. Koko Eating Bananas
 
     static void main() {
-        int[] piles = {3,6,7,11};
-        int h = 8;
+        int[] piles = {30,11,23,4,20};
+        int h = 5;
         System.out.println(eatingBananas(piles,h));
     }
     static int eatingBananas(int[] piles, int h){

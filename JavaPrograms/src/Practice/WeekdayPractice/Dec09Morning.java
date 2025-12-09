62e27325ec346696666d852cea529b92ff751c04
SwarajXO <purarkarswaraj@gmail.com>
2025-12-09 05:43:20 +0530

    09-12-25  Practice 875. Koko Eating Bananas Test-1

--- JavaPrograms/src/Practice/WeekdayPractice/Dec09Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec09Morning.java
@@ -1,43 +1,76 @@
 package Practice.WeekdayPractice;
 
 public class Dec09Morning {
-/// 1011. Capacity To Ship Packages Within D Days
+    /// 875. Koko Eating Bananas
+
     static void main() {
-        int[] weights = {1,2,3,1,1};
-        int days = 4;
-        System.out.println(capacityShip(weights,days));
+        int[] piles = {3,6,7,11};
+        int h = 8;
+        System.out.println(eatingBananas(piles,h));
     }
-    static int capacityShip(int[] weights, int days){
-        int low = max(weights);
-        int high = sum(weights);
+    static int eatingBananas(int[] piles, int h){
+        int low = 0;
+        int high = max(piles);
         int ans = high;
         while (low<=high){
-            int mid = low+(high-low)/2;
-            if (canShip(weights,days,mid)){
-                ans = mid;
-                high = mid-1;
-            } else low = mid+1;
+            int speed = low+(high-low)/2;
+            if (canEat(piles,h,speed)){
+                ans = speed;
+                high = speed-1;
+            } else low = speed+1;
         }
         return ans;
     }
-    static boolean canShip(int[] weights, int days, int can){
-        int d = 1, load = 0;
-        for (int x: weights){
-            if (load+x>can){
-                d++;
-                load=x;
-            } else load+=x;
+    static boolean canEat(int[] piles, int h, int speed){
+        int hoursUsed = 0;
+        for (int pile:piles){
+            hoursUsed += (pile+speed-1)/speed;
+            if (hoursUsed>h) return false;
         }
-        return days >=d;
+        return true;
     }
     static int max(int[]arr){
         int m =0;
         for (int x:arr)m=Math.max(m,x);
         return m;
     }
-    static int sum(int[] arr){
-        int s= 0;
-        for (int x: arr)s+=x;
-        return s;
-    }
+/// 1011. Capacity To Ship Packages Within D Days
+//    static void main() {
+//        int[] weights = {1,2,3,1,1};
+//        int days = 4;
+//        System.out.println(capacityShip(weights,days));
+//    }
+//    static int capacityShip(int[] weights, int days){
+//        int low = max(weights);
+//        int high = sum(weights);
+//        int ans = high;
+//        while (low<=high){
+//            int mid = low+(high-low)/2;
+//            if (canShip(weights,days,mid)){
+//                ans = mid;
+//                high = mid-1;
+//            } else low = mid+1;
+//        }
+//        return ans;
+//    }
+//    static boolean canShip(int[] weights, int days, int can){
+//        int d = 1, load = 0;
+//        for (int x: weights){
+//            if (load+x>can){
+//                d++;
+//                load=x;
+//            } else load+=x;
+//        }
+//        return days >=d;
+//    }
+//    static int max(int[]arr){
+//        int m =0;
+//        for (int x:arr)m=Math.max(m,x);
+//        return m;
+//    }
+//    static int sum(int[] arr){
+//        int s= 0;
+//        for (int x: arr)s+=x;
+//        return s;
+//    }
 }

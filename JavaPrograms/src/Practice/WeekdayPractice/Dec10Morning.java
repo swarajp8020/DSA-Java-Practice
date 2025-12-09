11107e8da2a8a6ab2bec48684f5c3ff5a76ed25c
SwarajXO <purarkarswaraj@gmail.com>
2025-12-10 05:26:57 +0530

    10-12-25  Morning Practice 1011. 875. Koko Eating Bananas Test case-2

--- JavaPrograms/src/Practice/WeekdayPractice/Dec10Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec10Morning.java
@@ -4,8 +4,8 @@ public class Dec10Morning {
 
     /// 875. Koko Eating Bananas
     static void main() {
-        int[] piles = {3,6,7,11};
-        int h = 8;
+        int[] piles = {30,11,23,4,20};
+        int h = 5;
         System.out.println(minEatingSpeed(piles,h));
     }
     static int minEatingSpeed(int[] piles, int h){

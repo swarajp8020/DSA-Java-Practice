6f8562bb8a99862d75977cebb373756ee15df56a
SwarajXO <purarkarswaraj@gmail.com>
2025-12-10 06:04:42 +0530

    10-12-25  Morning Practice 199. Binary Tree Right Side View Test case-1

--- JavaPrograms/src/Practice/WeekdayPractice/Dec10Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec10Morning.java
@@ -43,38 +43,67 @@ import java.util.*;
 //        return 1 + Math.max(left,right);
 //    }
 //}
-class Solution3 {
-    public List<List<Integer>> levelOrder(TreeNode root){
-        List<List<Integer>> result = new ArrayList<>();
+//class Solution3 {
+//    public List<List<Integer>> levelOrder(TreeNode root){
+//        List<List<Integer>> result = new ArrayList<>();
+//        if (root == null) return result;
+//        Queue<TreeNode> q = new LinkedList<>();
+//        q.add(root);
+//        while (!q.isEmpty()) {
+//            int size = q.size();
+//            List<Integer> level = new ArrayList<>();
+//            for (int i = 0; i < size; i++) {
+//                TreeNode node = q.poll();
+//                level.add(node.val);
+//                if (node.left!=null)q.add(node.left);
+//                if (node.right!=null)q.add(node.right);
+//            }
+//            result.add(level);
+//        }
+//        return result;
+//    }
+//}
+class Solution34 {
+        public List<Integer> rightSideView(TreeNode root){
+        List<Integer> result = new ArrayList<>();
         if (root == null) return result;
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
         while (!q.isEmpty()) {
             int size = q.size();
-            List<Integer> level = new ArrayList<>();
+            TreeNode last = null;
             for (int i = 0; i < size; i++) {
                 TreeNode node = q.poll();
-                level.add(node.val);
+                last = node;
                 if (node.left!=null)q.add(node.left);
                 if (node.right!=null)q.add(node.right);
             }
-            result.add(level);
+            result.add(last.val);
         }
         return result;
     }
 }
 public class Dec10Morning {
-
-    /// 102. Binary Tree Level Order Traversal
+    /// 199. Binary Tree Right Side View
     static void main() {
         TreeNode node = new TreeNode(5);
         node.left = new TreeNode(4);
         node.right = new TreeNode(3);
         node.left.left = new TreeNode(2);
         node.left.right = new TreeNode(1);
-        Solution3 sol = new Solution3();
-        System.out.println(sol.levelOrder(node));
+        Solution34 sol = new Solution34();
+        System.out.println(sol.rightSideView(node));
     }
+    /// 102. Binary Tree Level Order Traversal
+//    static void main() {
+//        TreeNode node = new TreeNode(5);
+//        node.left = new TreeNode(4);
+//        node.right = new TreeNode(3);
+//        node.left.left = new TreeNode(2);
+//        node.left.right = new TreeNode(1);
+//        Solution3 sol = new Solution3();
+//        System.out.println(sol.levelOrder(node));
+//    }
 
     /// 104. Maximum Depth of Binary Tree
 //    static void main() {

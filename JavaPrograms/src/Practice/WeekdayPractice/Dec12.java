96ae31d4f274d5218f0dd0654995fbcf2c3b5434
SwarajXO <purarkarswaraj@gmail.com>
2025-12-12 06:54:29 +0530

    12-12-25 The big revision list quickSortMedian, quickSortRandom, KthLargest, KthSmallest,TopKLargest, TopKSmallest

--- JavaPrograms/src/Practice/WeekdayPractice/Dec12.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec12.java
@@ -174,18 +174,58 @@ public class Dec12 {
 //        arr[a] = arr[b];
 //        arr[b] = temp;
 //    }
-    /// TopKLargest
+//    /// TopKLargest
+//    static void main() {
+//        int[] arr = {54,2,3,5,6,22};
+//        int k =5;
+//        int[] result = topKLargest(arr,k);
+//        System.out.println(Arrays.toString(result));
+//    }
+//    static int[] topKLargest(int[]arr, int k){
+//        int n = arr.length;
+//        int targetIndex = n-k;
+//        quickSelect(arr, 0, n-1, targetIndex);
+//        return Arrays.copyOfRange(arr, targetIndex,n);
+//    }
+//    static int quickSelect(int[] arr, int left, int right, int targetIndex){
+//        if (left<=right){
+//            int pivotIndex = partition(arr, left, right);
+//            if (pivotIndex==targetIndex)return arr[pivotIndex];
+//            if (pivotIndex<targetIndex)return quickSelect(arr, pivotIndex+1,right,targetIndex);
+//            else return quickSelect(arr,left,pivotIndex-1,targetIndex);
+//        }
+//        return 0;
+//    }
+//    static int partition(int[] arr, int left, int right){
+//        int pivot = arr[right];
+//        int i = left-1;
+//        for (int j = left; j < right; j++) {
+//            if (arr[j]<pivot){
+//                i++;
+//                swap(arr,i,j);
+//            }
+//        }
+//        swap(arr,i+1,right);
+//        return i+1;
+//    }
+//    static void swap(int[] arr, int a, int b){
+//        int temp = arr[a];
+//        arr[a] = arr[b];
+//        arr[b] = temp;
+//    }
+
+    /// TopKSmallest
     static void main() {
-        int[] arr = {54,2,3,5,6,22};
-        int k =5;
-        int[] result = topKLargest(arr,k);
+        int[] arr = {12,4,5,2,13,52};
+        int k = 5;
+        int[] result = topKSmallest(arr,k);
         System.out.println(Arrays.toString(result));
     }
-    static int[] topKLargest(int[]arr, int k){
+    static int[] topKSmallest(int[]arr, int k){
         int n = arr.length;
-        int targetIndex = n-k;
+        int targetIndex = k-1;
         quickSelect(arr, 0, n-1, targetIndex);
-        return Arrays.copyOfRange(arr, targetIndex,n);
+        return Arrays.copyOfRange(arr, 0,k);
     }
     static int quickSelect(int[] arr, int left, int right, int targetIndex){
         if (left<=right){

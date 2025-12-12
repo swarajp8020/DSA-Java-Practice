066a97612902352eb19d1dd1201630eaef5f3694
SwarajXO <purarkarswaraj@gmail.com>
2025-12-12 05:43:07 +0530

    12-12-25 Practice /// 1011. Capacity To Ship Packages Within D Days Test-1

--- JavaPrograms/src/Practice/WeekdayPractice/Dec12.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec12.java
@@ -1,32 +1,72 @@
 package Practice.WeekdayPractice;
 
 public class Dec12 {
-    /// 4. Median of Two Sorted Arrays
+
+    /// 1011. Capacity To Ship Packages Within D Days
     static void main() {
-        int[] A = {1,2};
-        int[] B = {3,4};
-        System.out.println(findMedianSorted(A,B));
+        int[] weights = {1,2,3,4,5,6,7,8,9,10};
+        int days = 5;
+        System.out.println(findShipWithinDays(weights,days));
     }
-    static double findMedianSorted(int[]A,int[]B){
-        int n = A.length;
-        int m = B.length;
-        int low = 0, high = n;
-        while (low <= high) {
-            int cutA = (high+low)/2;
-            int cutB = (n+m+1)/2-cutA;
-            int Aleft = (cutA == 0) ? Integer.MIN_VALUE : A[cutA-1];
-            int Aright = (cutA == n) ? Integer.MAX_VALUE : A[cutA];
-            int Bleft = (cutB == 0) ? Integer.MIN_VALUE : B[cutB-1];
-            int Bright = (cutB == m) ? Integer.MAX_VALUE : B[cutB];
-            if (Aleft <= Bright && Bleft <= Aright) {
-                if ((n + m) % 2 == 0) {
-                    return (Math.max(Aleft,Bleft)+Math.min(Aright,Bright))/2.0;
-                }
-                return Math.max(Aleft,Bleft);
-            } else if (Aleft > Bright) {
-                high = cutA-1;
-            } else low = cutA+1;
+    static int findShipWithinDays(int[] weights, int days){
+        int low = max(weights);
+        int high = sum(weights);
+        int ans = high;
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if (canShip(weights,days,mid)){
+                ans = mid;
+                high = mid-1;
+            } else low = mid+1;
         }
-        return 0;
+        return ans;
     }
+    static boolean canShip(int[] weights, int days, int can){
+        int d=1, load=0;
+        for (int x:weights){
+            if (load + x > can) {
+                d++;
+                load=x;
+            }else load+=x;
+        }
+        return days >= d;
+    }
+    static int max(int[] arr){
+        int m = 0;
+        for (int x: arr)m = Math.max(m,x);
+        return m;
+    }
+    static int sum(int[] arr){
+        int s = 0;
+        for (int x:arr)s+=x;
+        return s;
+    }
+    /// 4. Median of Two Sorted Arrays
+//    static void main() {
+//        int[] A = {1,2};
+//        int[] B = {3,4};
+//        System.out.println(findMedianSorted(A,B));
+//    }
+//    static double findMedianSorted(int[]A,int[]B){
+//        int n = A.length;
+//        int m = B.length;
+//        int low = 0, high = n;
+//        while (low <= high) {
+//            int cutA = (high+low)/2;
+//            int cutB = (n+m+1)/2-cutA;
+//            int Aleft = (cutA == 0) ? Integer.MIN_VALUE : A[cutA-1];
+//            int Aright = (cutA == n) ? Integer.MAX_VALUE : A[cutA];
+//            int Bleft = (cutB == 0) ? Integer.MIN_VALUE : B[cutB-1];
+//            int Bright = (cutB == m) ? Integer.MAX_VALUE : B[cutB];
+//            if (Aleft <= Bright && Bleft <= Aright) {
+//                if ((n + m) % 2 == 0) {
+//                    return (Math.max(Aleft,Bleft)+Math.min(Aright,Bright))/2.0;
+//                }
+//                return Math.max(Aleft,Bleft);
+//            } else if (Aleft > Bright) {
+//                high = cutA-1;
+//            } else low = cutA+1;
+//        }
+//        return 0;
+//    }
 }

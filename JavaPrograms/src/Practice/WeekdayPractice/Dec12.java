bc06642cf1627f88f945b9f2b023e00a451d0eff
SwarajXO <purarkarswaraj@gmail.com>
2025-12-12 06:04:38 +0530

    12-12-25 Practice 102. Binary Tree Level Order Traversal Test-1

--- JavaPrograms/src/Practice/WeekdayPractice/Dec12.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec12.java
@@ -1,46 +1,88 @@
 package Practice.WeekdayPractice;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+class TimeMap6{
+    static HashMap<String , List<Pair5>> map;
+    static class Pair5{
+        int time;
+        String value;
+        Pair5(int t, String v){
+            time = t;
+            value = v;
+        }
+    }
+    public TimeMap6(){
+        map = new HashMap<>();
+    }
+    static  void set(String key, String value, int timestamp){
+        map.putIfAbsent(key, new ArrayList<>());
+        map.get(key).add(new Pair5(timestamp,value));
+    }
+    static String  get(String key, int timestamp){
+        if (!map.containsKey(key))return "";
+        List<Pair5> list = map.get(key);
+        int low = 0, high = list.size()-1;
+        String ans = "";
+        while (low<=high){
+            int mid = low+(high-low)/2;
+            if (list.get(mid).time<=timestamp){
+                ans = list.get(mid).value;
+                low = mid+1;
+            }else high = mid-1;
+        }
+        return ans;
+    }
+}
 public class Dec12 {
+    //102. Binary Tree Level Order Traversal
+    static void main() {
+        TimeMap6 level = new TimeMap6();
+        level.set("seo","Siuuu", 7);
+        System.out.println(level.get("seo",7));
+    }
 /// 410. Split Array Largest Sum
-static void main() {
-    int[] nums = {1,2,3,4,5};
-    int  k = 2;
-    System.out.println(splitArray(nums,k));
-}
-static int splitArray(int[]nums,int k){
-    int low = max(nums);
-    int high = sum(nums);
-    int ans = high;
-    while (low <= high) {
-        int mid = low+(high-low)/2;
-        if (canSplit(nums,k,mid)){
-            ans = mid;
-            high = mid-1;
-        } else low = mid+1;
-    }
-    return ans;
-}
-static boolean canSplit(int[] nums, int k, int limit){
-    int groupCount = 1, currentSum = 0;
-    for (int x:nums){
-        if (currentSum+x>limit){
-            groupCount++;
-            currentSum = x;
-            if (groupCount>k)return false;
-        } else currentSum += x;
-    }
-    return k>=groupCount;
-}
-    static int max(int[] arr){
-        int m = 0;
-        for (int x: arr)m = Math.max(m,x);
-        return m;
-    }
-    static int sum(int[] arr){
-        int s = 0;
-        for (int x:arr)s+=x;
-        return s;
-    }
+//static void main() {
+//    int[] nums = {1,2,3,4,5};
+//    int  k = 2;
+//    System.out.println(splitArray(nums,k));
+//}
+//static int splitArray(int[]nums,int k){
+//    int low = max(nums);
+//    int high = sum(nums);
+//    int ans = high;
+//    while (low <= high) {
+//        int mid = low+(high-low)/2;
+//        if (canSplit(nums,k,mid)){
+//            ans = mid;
+//            high = mid-1;
+//        } else low = mid+1;
+//    }
+//    return ans;
+//}
+//static boolean canSplit(int[] nums, int k, int limit){
+//    int groupCount = 1, currentSum = 0;
+//    for (int x:nums){
+//        if (currentSum+x>limit){
+//            groupCount++;
+//            currentSum = x;
+//            if (groupCount>k)return false;
+//        } else currentSum += x;
+//    }
+//    return k>=groupCount;
+//}
+//    static int max(int[] arr){
+//        int m = 0;
+//        for (int x: arr)m = Math.max(m,x);
+//        return m;
+//    }
+//    static int sum(int[] arr){
+//        int s = 0;
+//        for (int x:arr)s+=x;
+//        return s;
+//    }
 
     /// 875. Koko Eating Bananas
 //    static void main() {

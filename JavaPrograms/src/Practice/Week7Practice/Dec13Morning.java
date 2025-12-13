97a6a62ef9b823c368b4e333eb3b8d35501b6712
SwarajXO <purarkarswaraj@gmail.com>
2025-12-13 07:24:08 +0530

    13-12-25 revising topics LongestUniqueSubarray,

---
+++ JavaPrograms/src/Practice/Week7Practice/Dec13Morning.java
@@ -0,0 +1,28 @@
+package Practice.Week7Practice;
+
+public class Dec13Morning {
+    /// CountSubarraysWithSumX
+    static void main() {
+
+    }
+
+    /// LongestUniqueSubarray
+//    static void main() {
+//        int[] arr = {1,2,3,1,2,3,4};
+//        System.out.println(longestUnique(arr));
+//    }
+//    static int longestUnique(int[] arr){
+//        int[] temp = new int[1001];
+//        int left = 0;
+//        int maxLen = 0;
+//        for (int right = 0; right < arr.length; right++) {
+//            temp[arr[right]]++;
+//            if (temp[arr[right]] > 1) {
+//                temp[arr[left]]--;
+//                left++;
+//            }
+//            maxLen = Math.max(maxLen,right-left+1);
+//        }
+//        return maxLen;
+//    }
+}

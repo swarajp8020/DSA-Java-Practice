fa5fb10d07bc8d570b121235174fa574d389edf4
SwarajXO <purarkarswaraj@gmail.com>
2025-12-13 07:34:01 +0530

    13-12-25 revising topics - Sliding Windows LongestUniqueSubarray, CountSubarraysWithSumX

--- JavaPrograms/src/Practice/Week7Practice/Dec13Morning.java
+++ JavaPrograms/src/Practice/Week7Practice/Dec13Morning.java
@@ -3,7 +3,24 @@ package Practice.Week7Practice;
 public class Dec13Morning {
     /// CountSubarraysWithSumX
     static void main() {
-
+        int[] arr = {3,1,4,2,5,6};
+        int x = 3;
+        int res = countSubarrayWithSumX(arr,x);
+        System.out.println(res);
+    }
+    static int countSubarrayWithSumX(int[] arr, int x){
+        int left = 0;
+        int sum = 0;
+        int count = 0;
+        for (int right = 0; right < arr.length; right++) {
+            sum += arr[right];
+            while (sum>x){
+                sum -= arr[left];
+                left++;
+            }
+            if (sum == x) count++;
+        }
+        return count;
     }
 
     /// LongestUniqueSubarray

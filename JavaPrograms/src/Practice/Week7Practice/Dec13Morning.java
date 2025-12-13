de54228188668931279a4fc14d551a56ac668ee9
SwarajXO <purarkarswaraj@gmail.com>
2025-12-13 08:03:44 +0530

    13-12-25 revising topics - Sliding Windows LongestUniqueSubarray, CountSubarraysWithSumX, MaxSumSubarrayK, LongestSubArraySumX

--- JavaPrograms/src/Practice/Week7Practice/Dec13Morning.java
+++ JavaPrograms/src/Practice/Week7Practice/Dec13Morning.java
@@ -1,25 +1,44 @@
 package Practice.Week7Practice;
 
 public class Dec13Morning {
-    /// MaxSumSubarrayK
+    /// LongestSubArraySumX
     static void main() {
-        int[] arr = {3,5,1,5,1,2};
-        int k = 3;
-        System.out.println(maxSum(arr, k));
+        int[] arr = {2,1,5,1,3,2};
+        int x = 7;
+        int res = longestSubArraySum(arr,x);
+        System.out.println(res);
     }
-    static int maxSum(int[] arr, int k){
-        int maxSum = 0;
-        int windowSum = 0;
-        for (int i = 0; i < k; i++) {
-            windowSum += arr[i];
+    static  int longestSubArraySum(int[] arr, int x){
+        int maxLen = 0, left = 0, windowSum = 0;
+        for (int right = 0; right < arr.length; right++) {
+            windowSum += arr[right];
+            while (windowSum > x){
+                windowSum -= arr[left];
+                left++;
+            }
+            maxLen = Math.max(maxLen, right-left+1);
         }
-        maxSum = windowSum;
-        for (int i = k; i < arr.length; i++) {
-            windowSum += arr[i] - arr[i-k];
-            maxSum = Math.max(maxSum,windowSum);
-        }
-        return maxSum;
+        return maxLen;
     }
+    /// MaxSumSubarrayK
+//    static void main() {
+//        int[] arr = {3,5,1,5,1,2};
+//        int k = 3;
+//        System.out.println(maxSum(arr, k));
+//    }
+//    static int maxSum(int[] arr, int k){
+//        int maxSum = 0;
+//        int windowSum = 0;
+//        for (int i = 0; i < k; i++) {
+//            windowSum += arr[i];
+//        }
+//        maxSum = windowSum;
+//        for (int i = k; i < arr.length; i++) {
+//            windowSum += arr[i] - arr[i-k];
+//            maxSum = Math.max(maxSum,windowSum);
+//        }
+//        return maxSum;
+//    }
     /// CountSubarraysWithSumX
 //    static void main() {
 //        int[] arr = {3,1,4,2,5,6};

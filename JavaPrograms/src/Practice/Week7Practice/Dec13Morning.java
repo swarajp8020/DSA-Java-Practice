b48453dafd301502210feba521c249a13c770e3a
SwarajXO <purarkarswaraj@gmail.com>
2025-12-13 07:54:47 +0530

    13-12-25 revising topics - Sliding Windows LongestUniqueSubarray, CountSubarraysWithSumX, MaxSumSubarrayK

--- JavaPrograms/src/Practice/Week7Practice/Dec13Morning.java
+++ JavaPrograms/src/Practice/Week7Practice/Dec13Morning.java
@@ -1,27 +1,46 @@
 package Practice.Week7Practice;
 
 public class Dec13Morning {
-    /// CountSubarraysWithSumX
+    /// MaxSumSubarrayK
     static void main() {
-        int[] arr = {3,1,4,2,5,6};
-        int x = 3;
-        int res = countSubarrayWithSumX(arr,x);
-        System.out.println(res);
+        int[] arr = {3,5,1,5,1,2};
+        int k = 3;
+        System.out.println(maxSum(arr, k));
     }
-    static int countSubarrayWithSumX(int[] arr, int x){
-        int left = 0;
-        int sum = 0;
-        int count = 0;
-        for (int right = 0; right < arr.length; right++) {
-            sum += arr[right];
-            while (sum>x){
-                sum -= arr[left];
-                left++;
-            }
-            if (sum == x) count++;
+    static int maxSum(int[] arr, int k){
+        int maxSum = 0;
+        int windowSum = 0;
+        for (int i = 0; i < k; i++) {
+            windowSum += arr[i];
         }
-        return count;
+        maxSum = windowSum;
+        for (int i = k; i < arr.length; i++) {
+            windowSum += arr[i] - arr[i-k];
+            maxSum = Math.max(maxSum,windowSum);
+        }
+        return maxSum;
     }
+    /// CountSubarraysWithSumX
+//    static void main() {
+//        int[] arr = {3,1,4,2,5,6};
+//        int x = 3;
+//        int res = countSubarrayWithSumX(arr,x);
+//        System.out.println(res);
+//    }
+//    static int countSubarrayWithSumX(int[] arr, int x){
+//        int left = 0;
+//        int sum = 0;
+//        int count = 0;
+//        for (int right = 0; right < arr.length; right++) {
+//            sum += arr[right];
+//            while (sum>x){
+//                sum -= arr[left];
+//                left++;
+//            }
+//            if (sum == x) count++;
+//        }
+//        return count;
+//    }
 
     /// LongestUniqueSubarray
 //    static void main() {

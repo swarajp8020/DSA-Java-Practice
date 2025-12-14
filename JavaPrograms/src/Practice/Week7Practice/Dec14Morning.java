01e44bf35a40425f91dcce40d924158909e0ce33
SwarajXO <purarkarswaraj@gmail.com>
2025-12-14 05:33:57 +0530

    14-12-25 revision - sliding window longestSubarrayK

--- JavaPrograms/src/Practice/Week7Practice/Dec14Morning.java
+++ JavaPrograms/src/Practice/Week7Practice/Dec14Morning.java
@@ -7,19 +7,19 @@ public class Dec14Morning {
         int k = 2;
         System.out.println(longestSubarrayK(arr,k));
     }
-    static int longestSubarrayK(int[] arr, int k) {
+    static int longestSubarrayK(int[] arr, int k){
         int[] temp = new int[1001];
-        int unique = 0, left = 0, maxLen = 0;
+        int left =0,unique =0, maxLen =0;
         for (int right = 0; right < arr.length; right++) {
-            if (temp[arr[right]] == 0) unique++;
+            if (temp[arr[right]]==0) unique++;
             temp[arr[right]]++;
-            while (unique > k) {
+            while (unique>k){
                 temp[arr[left]]--;
-                if (temp[arr[left]] == 0) unique--;
+                if (temp[arr[left]]==0)unique--;
                 left++;
             }
-            if (unique == k) {
-                maxLen = Math.max(maxLen, right - left + 1);
+            if (unique==k){
+                maxLen=Math.max(maxLen,right-left+1);
             }
         }
         return maxLen;

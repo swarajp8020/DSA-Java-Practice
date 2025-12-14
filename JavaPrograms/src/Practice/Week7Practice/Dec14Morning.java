94106b6a769a1b86520e6c5754afd433d0bd902a
SwarajXO <purarkarswaraj@gmail.com>
2025-12-14 05:31:36 +0530

    14-12-25 revision longestSubarrayK

---
+++ JavaPrograms/src/Practice/Week7Practice/Dec14Morning.java
@@ -0,0 +1,27 @@
+package Practice.Week7Practice;
+
+public class Dec14Morning {
+/// LongestSubarrayKDistinct
+    static void main() {
+        int[] arr = {1,2,1,2,3,4,4,5};
+        int k = 2;
+        System.out.println(longestSubarrayK(arr,k));
+    }
+    static int longestSubarrayK(int[] arr, int k) {
+        int[] temp = new int[1001];
+        int unique = 0, left = 0, maxLen = 0;
+        for (int right = 0; right < arr.length; right++) {
+            if (temp[arr[right]] == 0) unique++;
+            temp[arr[right]]++;
+            while (unique > k) {
+                temp[arr[left]]--;
+                if (temp[arr[left]] == 0) unique--;
+                left++;
+            }
+            if (unique == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+        return maxLen;
+    }
+}

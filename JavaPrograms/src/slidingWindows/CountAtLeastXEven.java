713a8a94a6ac8cde07e58f9d44d45f89431d6243
SwarajXO <purarkarswaraj@gmail.com>
2025-12-14 06:09:35 +0530

    14-12-25 code fix

--- JavaPrograms/src/slidingWindows/CountAtLeastXEven.java
+++ JavaPrograms/src/slidingWindows/CountAtLeastXEven.java
@@ -13,7 +13,7 @@ public class CountAtLeastXEven {
         int evenCount =0;
         int count=0;
         for (int i = 0; i < k; i++) {
-            if (evenCount%2==0) evenCount++;
+            if (arr[i]%2==0) evenCount++;
         }
         if (evenCount>=x) count++;
         for (int i = k; i < arr.length; i++) {

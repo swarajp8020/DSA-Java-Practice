f575df48824dec4d8e660cd9ecbe9269d03c547c
SwarajXO <purarkarswaraj@gmail.com>
2025-12-14 05:59:37 +0530

    14-12-25 revision - sliding window longestSubarrayK, countEvenSubarray

--- JavaPrograms/src/Practice/Week7Practice/Dec14Morning.java
+++ JavaPrograms/src/Practice/Week7Practice/Dec14Morning.java
@@ -1,27 +1,51 @@
 package Practice.Week7Practice;
 
 public class Dec14Morning {
+/// countEvenSubarrays
+static void main() {
+    int[] arr = {1,4,6,2,2,15,2,1};
+    int k =3;
+    int x = 2;
+    int res = countEvenSubarray(arr,k,x);
+    System.out.println(res);
+}
+static int countEvenSubarray(int[] arr, int k, int x){
+    int evenCount = 0, count = 0;
+    for (int i = 0; i < k; i++) {
+        if (evenCount%2==0)evenCount++;
+    }
+    if (evenCount>=x)count++;
+    for (int i = k; i < arr.length; i++) {
+        if (arr[i-k]%2==0) evenCount--;
+        if (arr[i]%2==0) evenCount++;
+        if (evenCount>=x) count++;
+    }
+    return count;
+}
+
+
+
 /// LongestSubarrayKDistinct
-    static void main() {
-        int[] arr = {1,2,1,2,3,4,4,5};
-        int k = 2;
-        System.out.println(longestSubarrayK(arr,k));
-    }
-    static int longestSubarrayK(int[] arr, int k){
-        int[] temp = new int[1001];
-        int left =0,unique =0, maxLen =0;
-        for (int right = 0; right < arr.length; right++) {
-            if (temp[arr[right]]==0) unique++;
-            temp[arr[right]]++;
-            while (unique>k){
-                temp[arr[left]]--;
-                if (temp[arr[left]]==0)unique--;
-                left++;
-            }
-            if (unique==k){
-                maxLen=Math.max(maxLen,right-left+1);
-            }
-        }
-        return maxLen;
-    }
+//    static void main() {
+//        int[] arr = {1,2,1,2,3,4,4,5};
+//        int k = 2;
+//        System.out.println(longestSubarrayK(arr,k));
+//    }
+//    static int longestSubarrayK(int[] arr, int k){
+//        int[] temp = new int[1001];
+//        int left =0,unique =0, maxLen =0;
+//        for (int right = 0; right < arr.length; right++) {
+//            if (temp[arr[right]]==0) unique++;
+//            temp[arr[right]]++;
+//            while (unique>k){
+//                temp[arr[left]]--;
+//                if (temp[arr[left]]==0)unique--;
+//                left++;
+//            }
+//            if (unique==k){
+//                maxLen=Math.max(maxLen,right-left+1);
+//            }
+//        }
+//        return maxLen;
+//    }
 }

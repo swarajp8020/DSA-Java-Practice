9821abb299b1df0c5fd36a9af15b2ed548634de3
SwarajXO <purarkarswaraj@gmail.com>
2025-12-14 19:29:15 +0530

    14-12-25 String - ReverseString

--- JavaPrograms/src/string/practice.java
+++ JavaPrograms/src/string/practice.java
@@ -1,15 +1,21 @@
 package string;
 
 public class practice {
+
     static void main() {
         String s = "hello";
-        System.out.println(s);
-        System.out.println("Length of s: "+s.length());
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(s).reverse();
-        System.out.println(stringBuilder.toString());
-        for (int i = 0; i < s.length(); i++) {
-            System.out.println("Index"+i+"->"+s.charAt(i));
-        }
+
     }
+
+//    static void main() {
+//        String s = "hello";
+//        System.out.println(s);
+//        System.out.println("Length of s: "+s.length());
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append(s).reverse();
+//        System.out.println(stringBuilder.toString());
+//        for (int i = 0; i < s.length(); i++) {
+//            System.out.println("Index"+i+"->"+s.charAt(i));
+//        }
+//    }
 }

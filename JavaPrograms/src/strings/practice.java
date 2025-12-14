a2b0a7789bbbe50e0f0c95815c00eee077d9007e
SwarajXO <purarkarswaraj@gmail.com>
2025-12-14 11:05:10 +0530

    14-12-25 String

---
+++ JavaPrograms/src/strings/practice.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class practice {
+    static void main() {
+        String s = "hello";
+        System.out.println(s);
+        System.out.println("Length of s: "+s.length());
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(s).reverse();
+        System.out.println(stringBuilder.toString());
+    }
+}

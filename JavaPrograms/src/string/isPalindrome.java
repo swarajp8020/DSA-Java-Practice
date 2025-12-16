e41a277ba6ebafb28f7a960ecc8c02ebb28b99b0
SwarajXO <purarkarswaraj@gmail.com>
2025-12-16 08:08:00 +0530

    15-12-25 IsPalindrome for numeric

--- JavaPrograms/src/string/isPalindrome.java
+++ JavaPrograms/src/string/isPalindrome.java
@@ -2,20 +2,39 @@ package string;
 
 public class isPalindrome {
     static void main(String[] args) {
-        String s = "swaraj";
+        String s = "A man, a plan, a canal: Panama";
         isPalindrome i = new isPalindrome();
         System.out.println(i.isPalindrome(s));
     }
-    boolean isPalindrome(String s){
-        int left = 0;
-        int right = s.length()-1;
-        while (left < right) {
-            if (s.charAt(left)!=s.charAt(right)){
-                return false;
+    public boolean isPalindrome(String s){
+        StringBuilder clean = new StringBuilder();
+        for (char c: s.toCharArray()){
+            if (Character.isLetterOrDigit(c)){
+                clean.append(Character.toLowerCase(c));
             }
+        }
+        int left = 0, right = clean.length()-1;
+        while (left < right) {
+            if (clean.charAt(left)!=clean.charAt(right))return false;
             left++;
             right--;
         }
         return true;
     }
+
+
+
+    /// Simpler
+//    boolean isPalindrome(String s){
+//        int left = 0;
+//        int right = s.length()-1;
+//        while (left < right) {
+//            if (s.charAt(left)!=s.charAt(right)){
+//                return false;
+//            }
+//            left++;
+//            right--;
+//        }
+//        return true;
+//    }
 }

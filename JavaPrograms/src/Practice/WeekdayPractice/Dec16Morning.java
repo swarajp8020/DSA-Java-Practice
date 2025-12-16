a849fb280689a01a495db6827711aff06af7f1b8
SwarajXO <purarkarswaraj@gmail.com>
2025-12-16 07:11:12 +0530

    15-12-25 Frequency Map

--- JavaPrograms/src/Practice/WeekdayPractice/Dec16Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec16Morning.java
@@ -1,19 +1,33 @@
 package Practice.WeekdayPractice;
 
-public class Dec16Morning {
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
-    /// String Practice
-    static void main() {
-        String s = "Swaraj";
-        StringBuilder stringBuilder = new StringBuilder(s);
-        int left = 0, right = stringBuilder.length()-1;
-        while (left < right) {
-            char temp = stringBuilder.charAt(left);
-            stringBuilder.setCharAt(left, stringBuilder.charAt(right));
-            stringBuilder.setCharAt(right,temp);
-            left++;
-            right--;
+public class Dec16Morning {
+    /// Frequency Map
+    static void main(String[] args) {
+        String s = "leetcode";
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+            freq[index]++;
         }
-        System.out.println(stringBuilder.toString());
+        System.out.println(Arrays.toString(freq));
     }
+
+    /// String Reverse
+//    static void main() {
+//        String s = "Swaraj";
+//        StringBuilder stringBuilder = new StringBuilder(s);
+//        int left = 0, right = stringBuilder.length()-1;
+//        while (left < right) {
+//            char temp = stringBuilder.charAt(left);
+//            stringBuilder.setCharAt(left, stringBuilder.charAt(right));
+//            stringBuilder.setCharAt(right,temp);
+//            left++;
+//            right--;
+//        }
+//        System.out.println(stringBuilder.toString());
+//    }
 }

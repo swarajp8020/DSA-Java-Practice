2ed1e6c6b05722bd2a6f49f08bbd28bb1a3fb597
SwarajXO <purarkarswaraj@gmail.com>
2025-12-17 07:09:30 +0530

    17-12-25 String Practice reverse String, Frequency Array

--- JavaPrograms/src/Practice/WeekdayPractice/Dec17Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec17Morning.java
@@ -1,20 +1,33 @@
 package Practice.WeekdayPractice;
 
+import java.util.Arrays;
+
 public class Dec17Morning {
 
-    /// reverse String
+    /// Frequency Array
     static void main(String[] args) {
-        String s = "swaraj";
-        StringBuilder sb = new StringBuilder(s);
-        int left = 0, right = sb.length()-1;
-        while (left < right) {
-            char temp = sb.charAt(left);
-            sb.setCharAt(left, sb.charAt(right));
-            sb.setCharAt(right, temp);
-//            left++;
-            right--;
-            left++;
+        String s = "asav";
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int Index = s.charAt(i) - 'a';
+            freq[Index]++;
         }
-        System.out.println(sb.toString());
+        System.out.println(Arrays.toString(freq));
     }
+
+    /// reverse String
+//    static void main(String[] args) {
+//        String s = "swaraj";
+//        StringBuilder sb = new StringBuilder(s);
+//        int left = 0, right = sb.length()-1;
+//        while (left < right) {
+//            char temp = sb.charAt(left);
+//            sb.setCharAt(left, sb.charAt(right));
+//            sb.setCharAt(right, temp);
+////            left++;
+//            right--;
+//            left++;
+//        }
+//        System.out.println(sb.toString());
+//    }
 }

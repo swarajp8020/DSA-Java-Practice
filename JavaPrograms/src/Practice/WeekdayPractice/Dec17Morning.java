4b74e0d1fb412bc449d703c5ede2ddb33cddba49
SwarajXO <purarkarswaraj@gmail.com>
2025-12-17 07:31:19 +0530

    17-12-25 String Practice reverse String, Frequency Array, has duplicate, isPalindrome

--- JavaPrograms/src/Practice/WeekdayPractice/Dec17Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec17Morning.java
@@ -4,20 +4,36 @@ import java.util.Arrays;
 
 public class Dec17Morning {
 
-    /// has duplicate
+    /// isPalindrome
     static void main(String[] args) {
-        String s = "pqdv";
-        Dec17Morning dec = new Dec17Morning();
-        System.out.println(dec.hasDuplicate(s));
+        String s = "namaan";
+        Dec17Morning dc = new Dec17Morning();
+        System.out.println(dc.isPalindrome(s));
     }
-    boolean hasDuplicate(String s){
-        int[] freq = new int[26];
-        for (char c: s.toCharArray()){
-            if (freq[c-'a']>0) return true;
-            freq[c-'a']++;
+    boolean isPalindrome(String s){
+        int left = 0, right = s.length()-1;
+        while (left < right) {
+            if (s.charAt(left)!=s.charAt(right))return false;
+            left++;
+            right--;
         }
-        return false;
+        return true;
     }
+
+    /// has duplicate
+//    static void main(String[] args) {
+//        String s = "pqdv";
+//        Dec17Morning dec = new Dec17Morning();
+//        System.out.println(dec.hasDuplicate(s));
+//    }
+//    boolean hasDuplicate(String s){
+//        int[] freq = new int[26];
+//        for (char c: s.toCharArray()){
+//            if (freq[c-'a']>0) return true;
+//            freq[c-'a']++;
+//        }
+//        return false;
+//    }
     /// Frequency Array
 //    static void main(String[] args) {
 //        String s = "asav";

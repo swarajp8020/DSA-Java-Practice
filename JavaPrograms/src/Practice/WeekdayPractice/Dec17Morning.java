aa7238c725448b4fffd2eb475c39bb09fd98536c
SwarajXO <purarkarswaraj@gmail.com>
2025-12-17 07:24:04 +0530

    17-12-25 String Practice reverse String, Frequency Array, has duplicate

--- JavaPrograms/src/Practice/WeekdayPractice/Dec17Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec17Morning.java
@@ -4,16 +4,30 @@ import java.util.Arrays;
 
 public class Dec17Morning {
 
-    /// Frequency Array
+    /// has duplicate
     static void main(String[] args) {
-        String s = "asav";
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            int Index = s.charAt(i) - 'a';
-            freq[Index]++;
-        }
-        System.out.println(Arrays.toString(freq));
+        String s = "pqdv";
+        Dec17Morning dec = new Dec17Morning();
+        System.out.println(dec.hasDuplicate(s));
     }
+    boolean hasDuplicate(String s){
+        int[] freq = new int[26];
+        for (char c: s.toCharArray()){
+            if (freq[c-'a']>0) return true;
+            freq[c-'a']++;
+        }
+        return false;
+    }
+    /// Frequency Array
+//    static void main(String[] args) {
+//        String s = "asav";
+//        int[] freq = new int[26];
+//        for (int i = 0; i < s.length(); i++) {
+//            int Index = s.charAt(i) - 'a';
+//            freq[Index]++;
+//        }
+//        System.out.println(Arrays.toString(freq));
+//    }
 
     /// reverse String
 //    static void main(String[] args) {

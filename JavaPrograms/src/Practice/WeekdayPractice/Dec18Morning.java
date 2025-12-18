beb90947263ec53e0fecd04cf1c54f3f1832066d
SwarajXO <purarkarswaraj@gmail.com>
2025-12-18 06:52:22 +0530

    18-12-25 String Revision - PermutationInString, lengthOfLongestSubString

--- JavaPrograms/src/Practice/WeekdayPractice/Dec18Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec18Morning.java
@@ -3,38 +3,62 @@ package Practice.WeekdayPractice;
 import java.util.Arrays;
 
 public class Dec18Morning {
-    /// PermutationInString
+
+    /// lengthOfLongestSubstring
     static void main(String[] args) {
-        String s1 = "sad";
-        String s2 = "sadasda";
-        sol s = new sol();
-        System.out.println(s.checkInclusion(s1,s2));
+        String s = "asvavsaffg";
+        LongestSubstring ls = new LongestSubstring();
+        System.out.println(ls.lengthOfLongestSubString(s));
     }
-    static class sol{
-        public boolean checkInclusion(String s1, String s2){
-            if (s1.length()>s2.length())return false;
-            int[] freq1 = new int[26];
-            int[] freq2 = new int[26];
-            for (char c:s1.toCharArray()){
-                freq1[c-'a']++;
+    static class LongestSubstring{
+        public int lengthOfLongestSubString(String s) {
+            int[] freq = new int[128];
+            int left = 0, maxLen = 0;
+            for (int right = 0; right < s.length(); right++) {
+                char c = s.charAt(right);
+                freq[c-'a']++;
+                while (freq[c-'a']>1){
+                    freq[s.charAt(left)-'a']--;
+                    left++;
+                }
+                maxLen = Math.max(maxLen, right-left+1);
             }
-            int k = s1.length();
-            for (int i = 0; i < k; i++) {
-                freq2[s2.charAt(i)-'a']++;
-            }
-            if (matches(freq1,freq2))return true;
-            for (int i = k; i < s2.length(); i++) {
-                freq2[s2.charAt(i)-'a']--;
-                freq2[s2.charAt(i-k)-'a']++;
-                if (matches(freq1,freq2))return true;
-            }
-            return false;
-        }
-        static boolean matches(int[] a, int[] b){
-            for (int i = 0; i < 26; i++) {
-                if (a[i] != b[i]) return false;
-            }
-            return true;
+            return maxLen;
         }
     }
+
+    /// PermutationInString
+//    static void main(String[] args) {
+//        String s1 = "sad";
+//        String s2 = "sadasda";
+//        sol s = new sol();
+//        System.out.println(s.checkInclusion(s1,s2));
+//    }
+//    static class sol{
+//        public boolean checkInclusion(String s1, String s2){
+//            if (s1.length()>s2.length())return false;
+//            int[] freq1 = new int[26];
+//            int[] freq2 = new int[26];
+//            for (char c:s1.toCharArray()){
+//                freq1[c-'a']++;
+//            }
+//            int k = s1.length();
+//            for (int i = 0; i < k; i++) {
+//                freq2[s2.charAt(i)-'a']++;
+//            }
+//            if (matches(freq1,freq2))return true;
+//            for (int i = k; i < s2.length(); i++) {
+//                freq2[s2.charAt(i)-'a']--;
+//                freq2[s2.charAt(i-k)-'a']++;
+//                if (matches(freq1,freq2))return true;
+//            }
+//            return false;
+//        }
+//        static boolean matches(int[] a, int[] b){
+//            for (int i = 0; i < 26; i++) {
+//                if (a[i] != b[i]) return false;
+//            }
+//            return true;
+//        }
+//    }
 }

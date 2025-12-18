193c5846f1ff3b12ebc30e21c7edc33a5eb4c516
SwarajXO <purarkarswaraj@gmail.com>
2025-12-18 06:46:37 +0530

    18-12-25 String Revision - PermutationInString

--- JavaPrograms/src/Practice/WeekdayPractice/Dec18Morning.java
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec18Morning.java
@@ -12,10 +12,10 @@ public class Dec18Morning {
     }
     static class sol{
         public boolean checkInclusion(String s1, String s2){
-            if (s1.length()>s2.length()) return false;
+            if (s1.length()>s2.length())return false;
             int[] freq1 = new int[26];
             int[] freq2 = new int[26];
-            for (char c : s1.toCharArray()){
+            for (char c:s1.toCharArray()){
                 freq1[c-'a']++;
             }
             int k = s1.length();
@@ -24,8 +24,8 @@ public class Dec18Morning {
             }
             if (matches(freq1,freq2))return true;
             for (int i = k; i < s2.length(); i++) {
-                freq2[s2.charAt(i)-'c']--;
-                freq2[s2.charAt(i-k)-'c']++;
+                freq2[s2.charAt(i)-'a']--;
+                freq2[s2.charAt(i-k)-'a']++;
                 if (matches(freq1,freq2))return true;
             }
             return false;

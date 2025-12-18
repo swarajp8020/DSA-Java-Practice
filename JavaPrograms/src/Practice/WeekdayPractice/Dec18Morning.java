47179edba9b5a54d6039c75dbc77c4e2e92ae70e
SwarajXO <purarkarswaraj@gmail.com>
2025-12-18 06:42:54 +0530

    18-12-25 String Revision - PermutationInString

---
+++ JavaPrograms/src/Practice/WeekdayPractice/Dec18Morning.java
@@ -0,0 +1,40 @@
+package Practice.WeekdayPractice;
+
+import java.util.Arrays;
+
+public class Dec18Morning {
+    /// PermutationInString
+    static void main(String[] args) {
+        String s1 = "sad";
+        String s2 = "sadasda";
+        sol s = new sol();
+        System.out.println(s.checkInclusion(s1,s2));
+    }
+    static class sol{
+        public boolean checkInclusion(String s1, String s2){
+            if (s1.length()>s2.length()) return false;
+            int[] freq1 = new int[26];
+            int[] freq2 = new int[26];
+            for (char c : s1.toCharArray()){
+                freq1[c-'a']++;
+            }
+            int k = s1.length();
+            for (int i = 0; i < k; i++) {
+                freq2[s2.charAt(i)-'a']++;
+            }
+            if (matches(freq1,freq2))return true;
+            for (int i = k; i < s2.length(); i++) {
+                freq2[s2.charAt(i)-'c']--;
+                freq2[s2.charAt(i-k)-'c']++;
+                if (matches(freq1,freq2))return true;
+            }
+            return false;
+        }
+        static boolean matches(int[] a, int[] b){
+            for (int i = 0; i < 26; i++) {
+                if (a[i] != b[i]) return false;
+            }
+            return true;
+        }
+    }
+}

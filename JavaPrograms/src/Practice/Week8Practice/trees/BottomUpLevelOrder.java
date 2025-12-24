a4b656b0386a9163d510a96bfda9325cfaf32b47
SwarajXO <purarkarswaraj@gmail.com>
2025-12-24 06:56:43 +0530

    24-12-25 Revision Topic - Trees BinaryTreePathsSolution, MaxPathSum, ZigzagSolution, BottomUpLevelOrder

---
+++ JavaPrograms/src/Practice/Week8Practice/trees/BottomUpLevelOrder.java
@@ -0,0 +1,38 @@
+package Practice.Week8Practice.trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BottomUpLevelOrder {
+    public List<List<Integer>> levelOrderBottom(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Queue <TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                level.add(node.value);
+                if (node.left!=null)q.add(node.left);
+                if (node.right!=null)q.add(node.right);
+            }
+            result.add(0,level);
+        }
+        return result;
+    }
+
+    static void main(String[] args) {
+         TreeNode node = new TreeNode(5);
+        node.left = new TreeNode(4);
+        node.right = new TreeNode(3);
+        node.left.left = new TreeNode(2);
+        node.left.right = new TreeNode(1);
+
+        BottomUpLevelOrder sol = new BottomUpLevelOrder();
+        System.out.println(sol.levelOrderBottom(node));
+    }
+}

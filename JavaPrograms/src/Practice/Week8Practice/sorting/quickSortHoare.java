d5461e90add69ee630d98b6c0c683aae352ec15c
SwarajXO <purarkarswaraj@gmail.com>
2025-12-25 08:37:50 +0530

    25-12-25 Revision Topic - Sorting bubbleSort, insertionSort, selectionSort, mergeSort, mergeTwoSortedArrays, quickSort partitionLomuto, quickSortHoare

---
+++ JavaPrograms/src/Practice/Week8Practice/sorting/quickSortHoare.java
@@ -0,0 +1,32 @@
+package Practice.Week8Practice.sorting;
+
+import java.util.Arrays;
+
+public class quickSortHoare {
+    static void main(String[] args) {
+        int[] arr = {7, 5, 8, 2, 4, 1};
+        quickSortHoare(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void quickSortHoare(int[] arr, int left, int right){
+        if (left >= right)return;
+        int pivotIndex = partition(arr, left, right);
+        quickSortHoare(arr, left, pivotIndex);
+        quickSortHoare(arr, pivotIndex + 1, right);
+    }
+    static int partition(int[] arr, int left, int right){
+        int pivot = arr[(right+left)/2];
+        while (true) {
+            while (arr[left] < pivot) left++;
+            while (arr[right] > pivot) right--;
+            if (left >= right) return right;
+
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}

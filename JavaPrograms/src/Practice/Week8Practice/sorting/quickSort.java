2c9af351920bfd2386e94083f0b8c357fa62f19f
SwarajXO <purarkarswaraj@gmail.com>
2025-12-25 08:14:48 +0530

    25-12-25 Revision Topic - Sorting bubbleSort, insertionSort, selectionSort, mergeSort, mergeTwoSortedArrays, quickSort partitionLomuto

---
+++ JavaPrograms/src/Practice/Week8Practice/sorting/quickSort.java
@@ -0,0 +1,34 @@
+package Practice.Week8Practice.sorting;
+
+import java.util.Arrays;
+
+public class quickSort {
+    static void main(String[] args) {
+        int[] arr = {9,6,4,2};
+        quickSort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void quickSort(int[] arr, int left, int right){
+        if (left >= right) return;
+        int pivotIndex = partitionLomuto(arr, left, right);
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    static int partitionLomuto(int[] arr, int left, int right){
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, right);
+        return i+1;
+    }
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}

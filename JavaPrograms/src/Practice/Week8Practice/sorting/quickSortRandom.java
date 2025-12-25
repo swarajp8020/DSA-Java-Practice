414d65b3529e4f19357b3dbd26fc1d78154a520a
SwarajXO <purarkarswaraj@gmail.com>
2025-12-25 13:12:07 +0530

    25-12-25 Revision Topic - Sorting bubbleSort, insertionSort, selectionSort, mergeSort, mergeTwoSortedArrays, quickSort partitionLomuto, quickSortHoare, quickSortDNF, quickSortMedian, quickSortRandom

---
+++ JavaPrograms/src/Practice/Week8Practice/sorting/quickSortRandom.java
@@ -0,0 +1,36 @@
+package Practice.Week8Practice.sorting;
+
+import java.util.Arrays;
+
+public class quickSortRandom {
+    static void main(String[] args) {
+        int[] arr = {7, 2, 9, 4, 1, 8, 3};
+        quickSortRandom(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void quickSortRandom(int[] arr, int left, int right){
+        if (left >= right)return;
+        int randomIndex = left + (int) (Math.random()*(right-left+1));
+        swap(arr, randomIndex, right);
+        int pivotIndex = partition(arr, left, right);
+        quickSortRandom(arr, left, pivotIndex-1);
+        quickSortRandom(arr, pivotIndex+1, right);
+    }
+    static int partition(int[] arr, int left, int right){
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i+1, right);
+        return i+1;
+    }
+    static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}

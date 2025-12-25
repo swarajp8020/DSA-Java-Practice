43353cd1ea2a6b69d5a7ee1d96163727f6df2bf9
SwarajXO <purarkarswaraj@gmail.com>
2025-12-25 06:44:51 +0530

    25-12-25 Revision Topic - Sorting bubbleSort

--- JavaPrograms/src/Practice/Week8Practice/sorting/bubbleSort.java
+++ JavaPrograms/src/Practice/Week8Practice/sorting/bubbleSort.java
@@ -9,7 +9,7 @@ public class bubbleSort {
         System.out.println(Arrays.toString(arr));
     }
     static void bubbleSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j]>arr[j+1]){
                     int temp = arr[j];

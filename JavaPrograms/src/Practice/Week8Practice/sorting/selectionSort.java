af9a569ce0064aef0bc0f7ce182c8f0445dab0c0
SwarajXO <purarkarswaraj@gmail.com>
2025-12-25 07:13:42 +0530

    25-12-25 Revision Topic - Sorting bubbleSort, insertionSort, selectionSort

---
+++ JavaPrograms/src/Practice/Week8Practice/sorting/selectionSort.java
@@ -0,0 +1,29 @@
+package Practice.Week8Practice.sorting;
+
+import java.util.Arrays;
+/// Time: O(n²)
+/// Space: O(1)
+/// Stable: Insertion, Bubble
+/// Use case: nearly sorted arrays → insertion
+/// Interview line:
+/// “These are basic comparison sorts, mainly useful for understanding mechanics or small / nearly sorted inputs.”
+public class selectionSort {
+    static void main(String[] args) {
+        int[] arr={11,3,45,23,10,5,7};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+        }
+    }
+}

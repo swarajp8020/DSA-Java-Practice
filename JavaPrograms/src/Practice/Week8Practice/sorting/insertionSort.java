36ec5351ce04c1f8fa19ee99caf3f5ab550f3cd6
SwarajXO <purarkarswaraj@gmail.com>
2025-12-25 07:06:42 +0530

    25-12-25 Revision Topic - Sorting bubbleSort, insertionSort

---
+++ JavaPrograms/src/Practice/Week8Practice/sorting/insertionSort.java
@@ -0,0 +1,22 @@
+package Practice.Week8Practice.sorting;
+
+import java.util.Arrays;
+
+public class insertionSort {
+    static void main(String[] args) {
+        int[] arr ={2,4,1,5,23};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertionSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+}

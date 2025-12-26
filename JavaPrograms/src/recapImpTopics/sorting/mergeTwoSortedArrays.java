cf768a41526569deb070a7eb4800d51aef46989d
SwarajXO <purarkarswaraj@gmail.com>
2025-12-26 11:26:10 +0530

    26-12-25 Revision Topic - Sorting kth largest, KthSmallest, TopKLargest, TopKSmallest, kth largest, KthSmallest, TopKLargest, TopKSmallest, medianFinder, countInversions

---
+++ JavaPrograms/src/recapImpTopics/sorting/mergeTwoSortedArrays.java
@@ -0,0 +1,24 @@
+package recapImpTopics.sorting;
+
+import java.util.Arrays;
+
+public class mergeTwoSortedArrays {
+    static void main(String[] args) {
+        int[] a = {1,3,5,7};
+        int[] b = {2,4,6,8};
+        int[] res = merge(a, b);
+        System.out.println(Arrays.toString(res));
+    }
+    static int[] merge(int[] a, int[] b){
+        int[] temp = new int[a.length+ b.length];
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]){
+                temp[k++] = a[i++];
+            }else temp[k++] = b[j++];
+        }
+        while (i< a.length) temp[k++] = a[i++];
+        while (j <b.length) temp[k++] = b[j++];
+        return temp;
+    }
+}

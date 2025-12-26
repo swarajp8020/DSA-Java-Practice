98e90e83d26bafd345527f44cef242dfe96b23d0
SwarajXO <purarkarswaraj@gmail.com>
2025-12-26 11:25:09 +0530

    25-12-25 Revision Topic - Sorting kth largest, KthSmallest, TopKLargest, TopKSmallest, kth largest, KthSmallest, TopKLargest, TopKSmallest, medianFinder, countInversions

---
+++ JavaPrograms/src/recapImpTopics/sorting/insertionSort.java
@@ -0,0 +1,27 @@
+package recapImpTopics.sorting;
+
+import java.util.Arrays;
+/// Time: O(n²)
+/// Space: O(1)
+/// Stable: Insertion, Bubble
+/// Use case: nearly sorted arrays → insertion
+/// Interview line:
+/// “These are basic comparison sorts, mainly useful for understanding mechanics or small / nearly sorted inputs.”
+public class insertionSort {
+    static void main(String[] args) {
+        int[] arr ={2,4,1,5,23};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertionSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+}

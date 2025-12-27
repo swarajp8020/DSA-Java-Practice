4371629782ad144eff486b03f191b24457fed8ab
SwarajXO <purarkarswaraj@gmail.com>
2025-12-27 08:14:30 +0530

    27-12-25 Revision Topic - string LongestSubstringWithoutRepeatingCharacters, Permutation in String

--- JavaPrograms/src/Practice/Week8Practice/Dec27Morning.java
+++ JavaPrograms/src/Practice/Week8Practice/Dec27Morning.java
@@ -1,26 +1,61 @@
 package Practice.Week8Practice;
 
 public class Dec27Morning {
-    /// LongestSubstringWithoutRepeatingCharacters
+    /// Permutation in String
     static void main(String[] args) {
-        String s= "adadasf";
+        String s1 = "ab";
+        String s2 = "eidbaooo";
         Solution solution = new Solution();
-        System.out.println(solution.longestSubString(s));
+        System.out.println(solution.checkInclusion(s1,s2));
     }
     static class Solution{
-        public int longestSubString(String s){
-            int[] freq = new int[128];
-            int left = 0, maxLen = 0;
-            for (int right = 0; right < s.length(); right++) {
-                char c = s.charAt(right);
-                freq[c]++;
-                while (freq[c] > 1) {
-                    freq[s.charAt(left)]--;
-                    left++;
-                }
-                maxLen = Math.max(maxLen, right-left+1);
+        public boolean checkInclusion(String s1, String s2){
+            if (s1.length() > s2.length()) return false;
+            int[] freq1 = new int[26];
+            int[] freq2 = new int[26];
+            for (char c: s2.toCharArray()){
+                freq1[c - 'a']++;
             }
-            return maxLen;
+            int k = s1.length();
+            for (int i = 0; i < k; i++) {
+                freq2[s2.charAt(i) - 'a']++;
+            }
+            if (matches(freq1, freq2)) return true;
+            for (int right = k; right < s2.length(); right++) {
+                freq2[s2.charAt(right) - 'a']++;
+                freq2[s2.charAt(right - k) - 'a']--;
+                if (matches(freq1, freq2)) return true;
+            }
+            return false;
+        }
+        private boolean matches(int[] a, int[] b){
+            for (int i = 0; i < 26; i++) {
+                if (a[i] != b[i]) return true;
+            }
+            return false;
         }
     }
+
+    /// LongestSubstringWithoutRepeatingCharacters
+//    static void main(String[] args) {
+//        String s= "adadasf";
+//        Solution solution = new Solution();
+//        System.out.println(solution.longestSubString(s));
+//    }
+//    static class Solution{
+//        public int longestSubString(String s){
+//            int[] freq = new int[128];
+//            int left = 0, maxLen = 0;
+//            for (int right = 0; right < s.length(); right++) {
+//                char c = s.charAt(right);
+//                freq[c]++;
+//                while (freq[c] > 1) {
+//                    freq[s.charAt(left)]--;
+//                    left++;
+//                }
+//                maxLen = Math.max(maxLen, right-left+1);
+//            }
+//            return maxLen;
+//        }
+//    }
 }

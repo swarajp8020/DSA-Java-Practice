68b83ae8ef56175a3b3bbac6dd1bd000578ff21f
SwarajXO <purarkarswaraj@gmail.com>
2025-12-27 09:11:49 +0530

    27-12-25 Revision Topic - string LongestSubstringWithoutRepeatingCharacters, Permutation in String, Longest Repeating Character Replacement

--- JavaPrograms/src/Practice/Week8Practice/Dec27Morning.java
+++ JavaPrograms/src/Practice/Week8Practice/Dec27Morning.java
@@ -1,40 +1,69 @@
 package Practice.Week8Practice;
 
+
 public class Dec27Morning {
-    /// Permutation in String
+
+    ///Longest Repeating Character Replacement
     static void main(String[] args) {
-        String s1 = "ab";
-        String s2 = "eidbaooo";
-        Solution solution = new Solution();
-        System.out.println(solution.checkInclusion(s1,s2));
+        String s = "sada";
+        int k = 2;
+        Solution sol = new Solution();
+        System.out.println(sol.characterReplacement(s,k));
     }
-    static class Solution{
-        public boolean checkInclusion(String s1, String s2){
-            if (s1.length() > s2.length()) return false;
-            int[] freq1 = new int[26];
-            int[] freq2 = new int[26];
-            for (char c: s2.toCharArray()){
-                freq1[c - 'a']++;
+    static class Solution {
+        public int characterReplacement(String s, int k){
+            int[] freq = new int[26];
+            int left = 0, maxFreq = 0, maxLen = 0;
+            for (int right = 0; right < s.length(); right++) {
+                char c = s.charAt(right);
+                freq[c-'a']++;
+                maxFreq = Math.max(maxFreq, freq[c-'a']);
+                int windowSize = right - left + 1;
+                int charsToChange = windowSize - maxFreq;
+                if (charsToChange > k){
+                    freq[s.charAt(left)]--;
+                    left++;
+                }
+                maxLen = Math.max(maxLen, right-left+1);
             }
-            int k = s1.length();
-            for (int i = 0; i < k; i++) {
-                freq2[s2.charAt(i) - 'a']++;
-            }
-            if (matches(freq1, freq2)) return true;
-            for (int right = k; right < s2.length(); right++) {
-                freq2[s2.charAt(right) - 'a']++;
-                freq2[s2.charAt(right - k) - 'a']--;
-                if (matches(freq1, freq2)) return true;
-            }
-            return false;
-        }
-        private boolean matches(int[] a, int[] b){
-            for (int i = 0; i < 26; i++) {
-                if (a[i] != b[i]) return true;
-            }
-            return false;
+            return maxLen;
         }
     }
+
+    /// Permutation in String
+//    static void main(String[] args) {
+//        String s1 = "ab";
+//        String s2 = "eidbaooo";
+//        Solution solution = new Solution();
+//        System.out.println(solution.checkInclusion(s1,s2));
+//    }
+//    static class Solution{
+//        public boolean checkInclusion(String s1, String s2){
+//            if (s1.length() > s2.length()) return false;
+//            int[] freq1 = new int[26];
+//            int[] freq2 = new int[26];
+//            for (char c: s2.toCharArray()){
+//                freq1[c - 'a']++;
+//            }
+//            int k = s1.length();
+//            for (int i = 0; i < k; i++) {
+//                freq2[s2.charAt(i) - 'a']++;
+//            }
+//            if (matches(freq1, freq2)) return true;
+//            for (int right = k; right < s2.length(); right++) {
+//                freq2[s2.charAt(right) - 'a']++;
+//                freq2[s2.charAt(right - k) - 'a']--;
+//                if (matches(freq1, freq2)) return true;
+//            }
+//            return false;
+//        }
+//        private boolean matches(int[] a, int[] b){
+//            for (int i = 0; i < 26; i++) {
+//                if (a[i] != b[i]) return true;
+//            }
+//            return false;
+//        }
+//    }
 
     /// LongestSubstringWithoutRepeatingCharacters
 //    static void main(String[] args) {

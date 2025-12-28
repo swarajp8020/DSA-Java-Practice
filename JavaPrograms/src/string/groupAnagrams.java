1a830f5e65b3ca14752196d02a54ee026e95efe1
SwarajXO <purarkarswaraj@gmail.com>
2025-12-28 08:22:36 +0530

    28-12-25 new topic String groupAnagrams

---
+++ JavaPrograms/src/string/groupAnagrams.java
@@ -0,0 +1,34 @@
+package string;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+///  ELI5 FIRST - Think like this:
+/// Every string has a DNA
+/// For anagrams, the DNA is the same
+/// Use that DNA as a key
+/// Put strings with the same key into the same bucket
+/// That’s it.
+public class groupAnagrams {
+    static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+    static List<List<String>> groupAnagrams(String[] strs){
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s: strs){
+            int[] freq = new int[26];
+            for (char c: s.toCharArray()){
+                freq[c - 'a']++;
+            }
+            StringBuilder key = new StringBuilder();
+            for (int count: freq){
+                key.append('#').append(count);
+            }
+            map.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}

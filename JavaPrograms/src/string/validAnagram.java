5c77da3d08cef8a3f1081da162021459323ff3fc
SwarajXO <purarkarswaraj@gmail.com>
2025-12-28 07:49:13 +0530

    28-12-25 new topic String validAnagram

---
+++ JavaPrograms/src/string/validAnagram.java
@@ -0,0 +1,23 @@
+package string;
+/// LC 242: Valid Anagram
+public class validAnagram {
+    static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+    static boolean isAnagram(String s, String t){
+        if (s.length() != t.length()) return false;
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[s.charAt(i) - 'a']--;
+        }
+        for (int count : freq){
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

f610b1675ffd5adf01bb861d02e86a87b8259e75
SwarajXO <purarkarswaraj@gmail.com>
2025-12-29 08:11:38 +0530

    29-12-25 new topic String LongestPalindromicSubstring practice

--- JavaPrograms/src/Practice/Week9Practice/Dec29.java
+++ JavaPrograms/src/Practice/Week9Practice/Dec29.java
@@ -7,13 +7,13 @@ public class Dec29 {
         System.out.println(longestPalindrome(s));
     }
     static String longestPalindrome(String s){
-        if (s == null || s.length() < 2) return s;
+        if (s == null || s.length() < 2)return s;
         int start = 0, maxLen = 0;
         for (int i = 0; i < s.length(); i++) {
             int len1 = expand(s, i, i);
-            int len2 = expand(s, i, i + 1);
+            int len2 = expand(s, i, i +1);
             int len = Math.max(len1, len2);
-            if (len > maxLen){
+            if (len > maxLen) {
                 maxLen = len;
                 start = i - (len - 1) / 2;
             }

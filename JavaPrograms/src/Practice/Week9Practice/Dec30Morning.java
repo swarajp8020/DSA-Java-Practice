a07f390b58750a2c958a9cc879df18a86b8814b8
SwarajXO <purarkarswaraj@gmail.com>
2025-12-30 06:44:29 +0530

    30-12-25 new topic String palindromicSubstrings, FrequencyArray, ReverseString, LongestPalindromicSubstring

--- JavaPrograms/src/Practice/Week9Practice/Dec30Morning.java
+++ JavaPrograms/src/Practice/Week9Practice/Dec30Morning.java
@@ -3,20 +3,46 @@ package Practice.Week9Practice;
 import java.util.Arrays;
 
 public class Dec30Morning {
-    /// ReverseString
+    /// LongestPalindromicSubstring
     static void main(String[] args) {
-        String s = "hello";
-        StringBuilder sb = new StringBuilder(s);
-        int left = 0, right = sb.length() - 1;
-        while (left < right) {
-            char temp = sb.charAt(left);
-            sb.setCharAt(left, sb.charAt(right));
-            sb.setCharAt(right, temp);
-            left++;
-            right--;
-        }
-        System.out.println(sb.toString());
+        String s = "babad";
+        System.out.println(longestPalindrome(s));
     }
+    static String longestPalindrome(String s){
+        if (s == null || s.length() < 2) return s;
+        int start = 0, maxLen = 1;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+    static int expand(String s, int left, int right){
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+    /// ReverseString
+//    static void main(String[] args) {
+//        String s = "hello";
+//        StringBuilder sb = new StringBuilder(s);
+//        int left = 0, right = sb.length() - 1;
+//        while (left < right) {
+//            char temp = sb.charAt(left);
+//            sb.setCharAt(left, sb.charAt(right));
+//            sb.setCharAt(right, temp);
+//            left++;
+//            right--;
+//        }
+//        System.out.println(sb.toString());
+//    }
 
     /// FrequencyArray
 //    static void main(String[] args) {

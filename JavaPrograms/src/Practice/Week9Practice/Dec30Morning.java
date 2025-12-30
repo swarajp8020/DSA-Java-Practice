8e97ddca2df132a0f9c3d73cf65f7f5429586cd1
SwarajXO <purarkarswaraj@gmail.com>
2025-12-30 06:14:36 +0530

    30-12-25 new topic String palindromicSubstrings, FrequencyArray, ReverseString

--- JavaPrograms/src/Practice/Week9Practice/Dec30Morning.java
+++ JavaPrograms/src/Practice/Week9Practice/Dec30Morning.java
@@ -3,16 +3,31 @@ package Practice.Week9Practice;
 import java.util.Arrays;
 
 public class Dec30Morning {
-    /// FrequencyArray
+    /// ReverseString
     static void main(String[] args) {
-        String s = "abc";
-        int[] freq = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            freq[c - 'a']++;
+        String s = "hello";
+        StringBuilder sb = new StringBuilder(s);
+        int left = 0, right = sb.length() - 1;
+        while (left < right) {
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+            left++;
+            right--;
         }
-        System.out.println(Arrays.toString(freq));
+        System.out.println(sb.toString());
     }
+
+    /// FrequencyArray
+//    static void main(String[] args) {
+//        String s = "abc";
+//        int[] freq = new int[26];
+//        for (int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            freq[c - 'a']++;
+//        }
+//        System.out.println(Arrays.toString(freq));
+//    }
 
     /// palindromicSubstrings
 //    static void main(String[] args) {

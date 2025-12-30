36afd679373a96a0dd20b72d5ad2a31d46de93c0
SwarajXO <purarkarswaraj@gmail.com>
2025-12-30 06:07:58 +0530

    30-12-25 new topic String palindromicSubstrings, FrequencyArray

--- JavaPrograms/src/Practice/Week9Practice/Dec30Morning.java
+++ JavaPrograms/src/Practice/Week9Practice/Dec30Morning.java
@@ -1,26 +1,39 @@
 package Practice.Week9Practice;
 
+import java.util.Arrays;
+
 public class Dec30Morning {
-    /// palindromicSubstrings
+    /// FrequencyArray
     static void main(String[] args) {
         String s = "abc";
-        System.out.println(countSubstrings(s));
-    }
-    static int countSubstrings(String s){
-        int count = 0;
+        int[] freq = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            count += expand(s, i, i);
-            count += expand(s, i, i + 1);
+            char c = s.charAt(i);
+            freq[c - 'a']++;
         }
-        return count;
+        System.out.println(Arrays.toString(freq));
     }
-    static int expand(String s, int left, int right){
-        int count = 0;
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-            count++;
-            left--;
-            right++;
-        }
-        return count;
-    }
+
+    /// palindromicSubstrings
+//    static void main(String[] args) {
+//        String s = "abc";
+//        System.out.println(countSubstrings(s));
+//    }
+//    static int countSubstrings(String s){
+//        int count = 0;
+//        for (int i = 0; i < s.length(); i++) {
+//            count += expand(s, i, i);
+//            count += expand(s, i, i + 1);
+//        }
+//        return count;
+//    }
+//    static int expand(String s, int left, int right){
+//        int count = 0;
+//        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+//            count++;
+//            left--;
+//            right++;
+//        }
+//        return count;
+//    }
 }

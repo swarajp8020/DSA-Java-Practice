6a4be6ad01ae172834bd8555e4e5555484073117
SwarajXO <purarkarswaraj@gmail.com>
2026-01-02 14:07:37 +0530

    02-01-26 ReverseLinkedlist

---
+++ JavaPrograms/src/Practice/Week9Practice/Jan02Afternoon.java
@@ -0,0 +1,35 @@
+package Practice.Week9Practice;
+
+class Solution2 {
+    public ListNode reverseList(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
+public class Jan02Afternoon {
+    /// ReverseLinkedlist
+    static void main(String[] args) {
+        ListNode head = new ListNode(5);
+        head.next = new ListNode(4);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(1);
+        Solution2 sol = new Solution2();
+        ListNode newHead = sol.reverseList(head);
+        printHead(newHead);
+    }
+    static void printHead(ListNode head){
+        while (head != null) {
+            System.out.print(head.val + "->");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+}

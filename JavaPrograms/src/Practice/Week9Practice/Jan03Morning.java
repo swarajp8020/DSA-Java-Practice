35ea40ed46c78d16a3ea84c9b82f626d19d3e3f2
SwarajXO <purarkarswaraj@gmail.com>
2026-01-03 18:28:49 +0530

    03-01-26 MergeTwoSortedLists practice

--- JavaPrograms/src/Practice/Week9Practice/Jan03Morning.java
+++ JavaPrograms/src/Practice/Week9Practice/Jan03Morning.java
@@ -35,33 +35,52 @@ package Practice.Week9Practice;
 //        return null;
 //    }
 //}
+//class Solution2 {
+//    public ListNode removeNthFromEnd(ListNode head, int n){
+//        ListNode dummy = new ListNode(0);
+//        dummy.next = head;
+//        ListNode slow = dummy;
+//        ListNode fast = dummy;
+//        for (int i = 0; i < n; i++) {
+//            fast = fast.next;
+//        }
+//        while (fast.next != null) {
+//            fast = fast.next;
+//            slow = slow.next;
+//        }
+//        slow.next = slow.next.next;
+//        return dummy.next;
+//    }
+//}
 class Solution2 {
-    public ListNode removeNthFromEnd(ListNode head, int n){
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
         ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode slow = dummy;
-        ListNode fast = dummy;
-        for (int i = 0; i < n; i++) {
-            fast = fast.next;
+        ListNode curr = dummy;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
         }
-        while (fast.next != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-        slow.next = slow.next.next;
+        curr.next = (list1 != null) ? list1 : list2;
         return dummy.next;
     }
 }
 public class Jan03Morning {
     static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
         Solution2 sol = new Solution2();
-        ListNode newHead = sol.removeNthFromEnd(head, 2);
-        printList(newHead);
+        ListNode merged = sol.mergeTwoLists(list1, list2);
+        printList(merged);
     }
     static void printList(ListNode head) {
         while (head != null) {
@@ -70,6 +89,23 @@ public class Jan03Morning {
         }
         System.out.println("null");
     }
+//    static void main(String[] args) {
+//        ListNode head = new ListNode(1);
+//        head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = new ListNode(4);
+//        head.next.next.next.next = new ListNode(5);
+//        Solution2 sol = new Solution2();
+//        ListNode newHead = sol.removeNthFromEnd(head, 2);
+//        printList(newHead);
+//    }
+//    static void printList(ListNode head) {
+//        while (head != null) {
+//            System.out.print(head.val + " → ");
+//            head = head.next;
+//        }
+//        System.out.println("null");
+//    }
 
 //    static void main(String[] args) {
 //        ListNode head = new ListNode(1);

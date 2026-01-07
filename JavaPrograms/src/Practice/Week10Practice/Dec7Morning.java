85105cf7be4d9f1d144d2116d9f536268f81dede
SwarajXO <purarkarswaraj@gmail.com>
2026-01-07 10:58:45 +0530

    07-01-26 Linkedlist Practice

--- JavaPrograms/src/Practice/Week10Practice/Dec7Morning.java
+++ JavaPrograms/src/Practice/Week10Practice/Dec7Morning.java
@@ -33,6 +33,7 @@ package Practice.Week10Practice;
 //}
 
 import java.awt.image.Kernel;
+import java.util.PriorityQueue;
 
 /// MiddleLinkedList
 //class Solution2 {
@@ -47,36 +48,63 @@ import java.awt.image.Kernel;
 //    }
 //}
 /// mergeTwoLists
+//class Solution2 {
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+//        ListNode dummy = new ListNode(0);
+//        ListNode curr = dummy;
+//        while (list1 != null && list2 != null) {
+//            if (list1.value <= list2.value) {
+//                curr.next = list1;
+//                list1 = list1.next;
+//            } else {
+//                curr.next = list2;
+//                list2 = list2.next;
+//            }
+//            curr = curr.next;
+//        }
+//        curr.next = (list1 != null) ? list1 : list2;
+//        return dummy.next;
+//    }
+//}
+/// MergeKSortedLists
 class Solution2 {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2){
+    public ListNode mergeKLists(ListNode[] lists){
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+                (a,b) -> a.value - b.value
+        );
+        for (ListNode node:lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
         ListNode dummy = new ListNode(0);
         ListNode curr = dummy;
-        while (list1 != null && list2 != null) {
-            if (list1.value <= list2.value) {
-                curr.next = list1;
-                list1 = list1.next;
-            } else {
-                curr.next = list2;
-                list2 = list2.next;
-            }
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            curr.next = node;
             curr = curr.next;
+            if (node.next != null) {
+                pq.offer(node.next);
+            }
         }
-        curr.next = (list1 != null) ? list1 : list2;
         return dummy.next;
     }
 }
 
-public class Dec7Morning {
 
+public class Dec7Morning {
     static void main(String[] args) {
-        ListNode list1 = new ListNode(1);
-        list1.next = new ListNode(2);
-        list1.next.next = new ListNode(4);
-        ListNode list2 = new ListNode(1);
-        list2.next = new ListNode(3);
-        list2.next.next = new ListNode(4);
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(5);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        ListNode l3 = new ListNode(2);
+        l3.next = new ListNode(6);
+        ListNode[] lists = {l1, l2, l3};
         Solution2 sol = new Solution2();
-        ListNode merged = sol.mergeTwoLists(list1, list2);
+        ListNode merged = sol.mergeKLists(lists);
         printList(merged);
     }
     static void printList(ListNode head) {
@@ -86,6 +114,24 @@ public class Dec7Morning {
         }
         System.out.println("null");
     }
+//    static void main(String[] args) {
+//        ListNode list1 = new ListNode(1);
+//        list1.next = new ListNode(2);
+//        list1.next.next = new ListNode(4);
+//        ListNode list2 = new ListNode(1);
+//        list2.next = new ListNode(3);
+//        list2.next.next = new ListNode(4);
+//        Solution2 sol = new Solution2();
+//        ListNode merged = sol.mergeTwoLists(list1, list2);
+//        printList(merged);
+//    }
+//    static void printList(ListNode head) {
+//        while (head != null) {
+//            System.out.print(head.value + " → ");
+//            head = head.next;
+//        }
+//        System.out.println("null");
+//    }
 //    static void main(String[] args) {
 //        ListNode head = new ListNode(1);
 //        head.next = new ListNode(2);

c9aacbb0df488fd15b3b4f3e337e10e2fb20a73f
SwarajXO <purarkarswaraj@gmail.com>
2026-01-09 08:32:48 +0530

    08-01-26 new topic - stack Valid Parenthesis practice

---
+++ JavaPrograms/src/Practice/Week10Practice/Dec9Morning.java
@@ -0,0 +1,26 @@
+package Practice.Week10Practice;
+
+import java.util.Stack;
+
+public class Dec9Morning {
+    public static boolean isValid(String s){
+        Stack <Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if (ch == ')' && top != '(') return false;
+                if (ch == '}' && top != '{') return false;
+                if (ch == ']' && top != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    static void main(String[] args) {
+        System.out.println(isValid("[{()}]"));
+        System.out.println(isValid("(]"));
+    }
+}

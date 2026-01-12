00c77a300916a9edfaa1cc4e2d3113a28929e2f0
SwarajXO <purarkarswaraj@gmail.com>
2026-01-12 09:34:19 +0530

    12-01-26 new topic - monotonicStack dailyTemperatures practice

--- JavaPrograms/src/Practice/Week11Practice/Dec12Morning.java
+++ JavaPrograms/src/Practice/Week11Practice/Dec12Morning.java
@@ -1,26 +1,46 @@
 package Practice.Week11Practice;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 public class Dec12Morning {
-    public static boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (char ch:s.toCharArray()) {
-            if (ch == '(' || ch == '{' || ch == '[') {
-                stack.push(ch);
-            } else {
-                if (stack.isEmpty()) return false;
-                char top = stack.pop();
-                if (ch == ')' && top != '(') return false;
-                if (ch == ']' && top != '[') return false;
-                if (ch == '}' && top != '{') return false;
+    public static int[] dailyTemperatures(int[] temprature) {
+        int n = temprature.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && temprature[i] > temprature[stack.peek()]) {
+                int prevIndex = stack.pop();
+                ans[prevIndex] = i - prevIndex;
             }
+            stack.push(i);
         }
-        return stack.isEmpty();
+        return ans;
     }
 
     static void main(String[] args) {
-        System.out.println(isValid("{[()]}"));
-        System.out.println(isValid("(]"));
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] result = dailyTemperatures(temperatures);
+        System.out.println(Arrays.toString(result));
     }
+//    public static boolean isValid(String s) {
+//        Stack<Character> stack = new Stack<>();
+//        for (char ch:s.toCharArray()) {
+//            if (ch == '(' || ch == '{' || ch == '[') {
+//                stack.push(ch);
+//            } else {
+//                if (stack.isEmpty()) return false;
+//                char top = stack.pop();
+//                if (ch == ')' && top != '(') return false;
+//                if (ch == ']' && top != '[') return false;
+//                if (ch == '}' && top != '{') return false;
+//            }
+//        }
+//        return stack.isEmpty();
+//    }
+//
+//    static void main(String[] args) {
+//        System.out.println(isValid("{[()]}"));
+//        System.out.println(isValid("(]"));
+//    }
 }

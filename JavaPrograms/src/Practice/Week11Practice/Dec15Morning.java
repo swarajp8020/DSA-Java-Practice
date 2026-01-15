b647c546f76a578a41b74a0af4db6beb75932bc7
SwarajXO <purarkarswaraj@gmail.com>
2026-01-15 13:15:58 +0530

    15-01-26 practice - stack Valid Parenthesis

--- JavaPrograms/src/Practice/Week11Practice/Dec15Morning.java
+++ JavaPrograms/src/Practice/Week11Practice/Dec15Morning.java
@@ -27,48 +27,70 @@ import java.util.Stack;
 //        return minStack.peek();
 //    }
 //}
-class MyQueue1 {
-    private Stack<Integer> inStack;
-    private Stack<Integer> outStack;
-    public MyQueue1() {
-        inStack = new Stack<>();
-        outStack = new Stack<>();
-    }
-    public void push(int x){
-        inStack.push(x);
-    }
-    public int pop(){
-        shiftStack();
-        return outStack.pop();
-    }
-    public int peek(){
-        shiftStack();
-        return outStack.peek();
-    }
-    public boolean empty(){
-        return outStack.isEmpty() && inStack.isEmpty();
-    }
-    public void shiftStack(){
-        if (outStack.isEmpty()) {
-            while (!inStack.isEmpty()) {
-                outStack.push(inStack.pop());
+//class MyQueue1 {
+//    private Stack<Integer> inStack;
+//    private Stack<Integer> outStack;
+//    public MyQueue1() {
+//        inStack = new Stack<>();
+//        outStack = new Stack<>();
+//    }
+//    public void push(int x){
+//        inStack.push(x);
+//    }
+//    public int pop(){
+//        shiftStack();
+//        return outStack.pop();
+//    }
+//    public int peek(){
+//        shiftStack();
+//        return outStack.peek();
+//    }
+//    public boolean empty(){
+//        return outStack.isEmpty() && inStack.isEmpty();
+//    }
+//    public void shiftStack(){
+//        if (outStack.isEmpty()) {
+//            while (!inStack.isEmpty()) {
+//                outStack.push(inStack.pop());
+//            }
+//        }
+//    }
+//}
+
+
+public class Dec15Morning {
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for (char ch:s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if (ch == ')' && top != '(') return false;
+                if (ch == '}' && top != '{') return false;
+                if (ch == ']' && top != '[') return false;
             }
         }
+        return stack.isEmpty();
     }
-}
-public class Dec15Morning {
+
     static void main(String[] args) {
-        MyQueue1 q = new MyQueue1();
-        q.push(1);
-        q.push(2);
-        q.push(3);
-        System.out.println(q.peek());  // 1
-        System.out.println(q.pop());   // 1
-        System.out.println(q.pop());   // 2
-        System.out.println(q.empty()); // false
-        System.out.println(q.pop());   // 3
-        System.out.println(q.empty()); // true
+        System.out.println(isValid("{[()]}"));
+        System.out.println(isValid("(}"));
     }
+//    static void main(String[] args) {
+//        MyQueue1 q = new MyQueue1();
+//        q.push(1);
+//        q.push(2);
+//        q.push(3);
+//        System.out.println(q.peek());  // 1
+//        System.out.println(q.pop());   // 1
+//        System.out.println(q.pop());   // 2
+//        System.out.println(q.empty()); // false
+//        System.out.println(q.pop());   // 3
+//        System.out.println(q.empty()); // true
+//    }
 //    static void main(String[] args) {
 //        MinStack1 ms = new MinStack1();
 //        ms.push(5);

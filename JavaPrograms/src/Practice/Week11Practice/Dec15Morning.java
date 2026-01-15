9a68ef8c9121cdc774fea3a251301c96de7d84f8
SwarajXO <purarkarswaraj@gmail.com>
2026-01-15 07:40:57 +0530

    15-01-26 new topic - monotonicStack MinStack practice

---
+++ JavaPrograms/src/Practice/Week11Practice/Dec15Morning.java
@@ -0,0 +1,44 @@
+package Practice.Week11Practice;
+
+import java.util.Stack;
+
+class MinStack1 {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+    public MinStack1(){
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+
+public class Dec15Morning {
+    static void main(String[] args) {
+        MinStack1 ms = new MinStack1();
+        ms.push(5);
+        ms.push(2);
+        ms.push(8);
+        System.out.println(ms.getMin());
+        System.out.println(ms.top());
+        ms.pop();
+        System.out.println(ms.top());
+        System.out.println(ms.getMin());
+    }
+}

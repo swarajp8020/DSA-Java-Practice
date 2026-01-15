3a884d5ba932c78f92ca80abc288e397e6500462
SwarajXO <purarkarswaraj@gmail.com>
2026-01-15 13:37:20 +0530

    15-01-26 practice - stack Valid Parenthesis, dailyTemperatures, nextGreaterElements

--- JavaPrograms/src/Practice/Week11Practice/Dec15Morning.java
+++ JavaPrograms/src/Practice/Week11Practice/Dec15Morning.java
@@ -1,4 +1,7 @@
 package Practice.Week11Practice;
+import stack.nextGreaterElements;
+
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Stack;
 
@@ -60,24 +63,45 @@ import java.util.Stack;
 
 
 public class Dec15Morning {
-    public static int[] dailyTemperatures(int[] tempratures){
-        int n = tempratures.length;
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
         int[] ans = new int[n];
-        Stack <Integer> stack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && tempratures[i] > tempratures[stack.peek()]) {
+        Arrays.fill(ans, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 2 * n; i++) {
+            int idx = i % n;
+            while (!stack.isEmpty() && nums[idx] > nums[stack.peek()]) {
                 int prevIndex = stack.pop();
-                ans[prevIndex] = i - prevIndex;
+                ans[prevIndex] = nums[idx];
             }
-            stack.push(i);
+            if (i < n) stack.push(idx);
         }
         return ans;
     }
     static void main(String[] args) {
-        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
-        int[] result = dailyTemperatures(temperatures);
-        System.out.println(Arrays.toString(result));
+        nextGreaterElements obj = new nextGreaterElements();
+        int[] nums = {1, 2, 1};
+        int[] result = obj.nextGreaterElements(nums);
+        System.out.println(Arrays.toString(result)); // Expected: [2, -1, 2]
     }
+//    public static int[] dailyTemperatures(int[] tempratures){
+//        int n = tempratures.length;
+//        int[] ans = new int[n];
+//        Stack <Integer> stack = new Stack<>();
+//        for (int i = 0; i < n; i++) {
+//            while (!stack.isEmpty() && tempratures[i] > tempratures[stack.peek()]) {
+//                int prevIndex = stack.pop();
+//                ans[prevIndex] = i - prevIndex;
+//            }
+//            stack.push(i);
+//        }
+//        return ans;
+//    }
+//    static void main(String[] args) {
+//        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+//        int[] result = dailyTemperatures(temperatures);
+//        System.out.println(Arrays.toString(result));
+//    }
 //    public static boolean isValid(String s){
 //        Stack<Character> stack = new Stack<>();
 //        for (char ch:s.toCharArray()) {

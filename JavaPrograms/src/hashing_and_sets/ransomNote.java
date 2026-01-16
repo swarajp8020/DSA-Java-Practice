09e4e73fc9efb4494ec083db1b35509b2228cefc
SwarajXO <purarkarswaraj@gmail.com>
2026-01-16 07:53:05 +0530

    16-01-26 Hashing and Sets ransomNote

--- JavaPrograms/src/hashing_and_sets/ransomNote.java
+++ JavaPrograms/src/hashing_and_sets/ransomNote.java
@@ -15,19 +15,15 @@ package hashing_and_sets;
 public class ransomNote {
     public static boolean canConstruct(String ransomNote, String magazine) {
         int[] freq = new int[26];
-
         for (char c : magazine.toCharArray()) {
             freq[c - 'a']++;
         }
-
         for (char c : ransomNote.toCharArray()) {
             freq[c - 'a']--;
             if (freq[c - 'a'] < 0) return false;
         }
-
         return true;
     }
-
     static void main(String[] args) {
         String ransom = "a";
         String magazine = "b";

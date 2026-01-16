79e8e326296ee34090b057663baba680f17711d5
SwarajXO <purarkarswaraj@gmail.com>
2026-01-16 08:26:13 +0530

    16-01-26 Hashing and Sets ransomNote practice

--- JavaPrograms/src/Practice/Week11Practice/Dec16Morning.java
+++ JavaPrograms/src/Practice/Week11Practice/Dec16Morning.java
@@ -24,50 +24,67 @@ import java.util.Stack;
 //    public int top(){return stack.peek();}
 //    public int getMin() {return minStack.peek();}
 //}
-class MyQueue1 {
-    private Stack<Integer> inStack;
-    private Stack<Integer> outStack;
-    public MyQueue1() {
-        inStack = new Stack<>();
-        outStack = new Stack<>();
-    }
-    public void push(int x) {
-        inStack.push(x);
-    }
-    public int pop(){
-        shiftStack();
-        return outStack.pop();
-    }
-    public int peek(){
-        shiftStack();
-        return outStack.peek();
-    }
-    public boolean empty(){
-        shiftStack();
-        return outStack.isEmpty() && inStack.isEmpty();
-    }
-    public void shiftStack() {
-        if (outStack.isEmpty()) {
-            while (!inStack.isEmpty()) {
-                outStack.push(inStack.pop());
-            }
-        }
-    }
-}
+//class MyQueue1 {
+//    private Stack<Integer> inStack;
+//    private Stack<Integer> outStack;
+//    public MyQueue1() {
+//        inStack = new Stack<>();
+//        outStack = new Stack<>();
+//    }
+//    public void push(int x) {
+//        inStack.push(x);
+//    }
+//    public int pop(){
+//        shiftStack();
+//        return outStack.pop();
+//    }
+//    public int peek(){
+//        shiftStack();
+//        return outStack.peek();
+//    }
+//    public boolean empty(){
+//        shiftStack();
+//        return outStack.isEmpty() && inStack.isEmpty();
+//    }
+//    public void shiftStack() {
+//        if (outStack.isEmpty()) {
+//            while (!inStack.isEmpty()) {
+//                outStack.push(inStack.pop());
+//            }
+//        }
+//    }
+//}
 
 public class Dec16Morning {
-    static void main(String[] args) {
-        MyQueue1 q = new MyQueue1();
-        q.push(1);
-        q.push(2);
-        q.push(3);
-        System.out.println(q.peek());  // 1
-        System.out.println(q.pop());   // 1
-        System.out.println(q.pop());   // 2
-        System.out.println(q.empty()); // false
-        System.out.println(q.pop());   // 3
-        System.out.println(q.empty()); // true
+
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+        for (char c:magazine.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (char c:ransomNote.toCharArray()) {
+            freq[c - 'a']--;
+            if (freq[c - 'a'] < 0) return false;
+        }
+        return true;
     }
+    static void main(String[] args) {
+        String ransom = "a";
+        String magazine = "b";
+        System.out.println(canConstruct(ransom, magazine));
+    }
+//    static void main(String[] args) {
+//        MyQueue1 q = new MyQueue1();
+//        q.push(1);
+//        q.push(2);
+//        q.push(3);
+//        System.out.println(q.peek());  // 1
+//        System.out.println(q.pop());   // 1
+//        System.out.println(q.pop());   // 2
+//        System.out.println(q.empty()); // false
+//        System.out.println(q.pop());   // 3
+//        System.out.println(q.empty()); // true
+//    }
 //    static void main(String[] args) {
 //        MinStack1 ms = new MinStack1();
 //        ms.push(5);

910f65aa49e49a6ad2211248dcc96a63b11aa58c
SwarajXO <purarkarswaraj@gmail.com>
2026-01-17 07:19:29 +0530

    17-01-26 Hashing and Sets longestConsecutiveSequence practice

--- JavaPrograms/src/Practice/Week11Practice/Dec17Morning.java
+++ JavaPrograms/src/Practice/Week11Practice/Dec17Morning.java
@@ -21,7 +21,7 @@ public class Dec17Morning {
         return longest;
     }
     static void main() {
-        int[] nums = {1,0,1,2};
+        int[] nums = {0,3,7,2,5,8,4,6,0,1};
         System.out.println(longestConsecutive(nums));
     }
 }

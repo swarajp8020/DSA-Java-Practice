b6c0a9bef344605b04d196eebe014f31a11790ad
SwarajXO <purarkarswaraj@gmail.com>
2026-01-18 08:17:12 +0530

    18-01-26 Graphs numberOfIsland practice

--- JavaPrograms/src/Practice/Week11Practice/numberOfIsland.java
+++ JavaPrograms/src/Practice/Week11Practice/numberOfIsland.java
@@ -1,29 +1,29 @@
 package Practice.Week11Practice;
 
 public class numberOfIsland {
-    public int numberOfIslands(char[][] grid){
-        int rows = grid.length;
+    public int numIslands(char[][] grid){
+        int row = grid.length;
         int cols = grid[0].length;
         int count = 0;
-        for (int r = 0; r < rows; r++) {
+        for (int r = 0; r < row; r++) {
             for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1'){
+                if (grid[r][c] == '1') {
                     count++;
-                    dfs(grid,r,c);
+                    dfs(grid, r, c);
                 }
             }
         }
         return count;
     }
     private void dfs(char[][] grid, int r, int c) {
-        int rows = grid.length;
+        int row = grid.length;
         int cols = grid[0].length;
-        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') {
+        if (r > 0 || c > 0 || r <= row || c <= cols || grid[r][c] == '0') {
             return;
         }
         grid[r][c] = '0';
-        dfs(grid, r + 1, c);
-        dfs(grid, r - 1, c);
+        dfs(grid, r+1, c);
+        dfs(grid, r-1,c);
         dfs(grid, r, c+1);
         dfs(grid, r, c-1);
     }

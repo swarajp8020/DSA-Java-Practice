1d9bd0faf3af801832bd33f781f5ddbb7c7f3913
SwarajXO <purarkarswaraj@gmail.com>
2026-01-19 08:05:57 +0530

    19-01-26 Graphs numIslands practice

--- JavaPrograms/src/Practice/Week12Practice/Jan19Morning.java
+++ JavaPrograms/src/Practice/Week12Practice/Jan19Morning.java
@@ -3,32 +3,32 @@ package Practice.Week12Practice;
 import graphs.numberOfIsland;
 
 public class Jan19Morning {
-    public int numIslands(char[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        int count = 0;
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1') {
-                    count++;
-                    dfs(grid, r, c);
-                }
-            }
-        }
-        return count;
-    }
-    private void dfs(char[][] grid, int r, int c) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') {
-            return;
-        }
-        grid[r][c] = '0';
-        dfs(grid, r+1,c);
-        dfs(grid, r-1,c);
-        dfs(grid, r, c+1);
-        dfs(grid, r, c-1);
-    }
+   public int numIslands(char[][] grid) {
+       int rows = grid.length;
+       int cols = grid[0].length;
+       int count = 0;
+       for (int r = 0; r < rows; r++) {
+           for (int c = 0; c < cols; c++) {
+               if (grid[r][c] == '1') {
+                   count++;
+                   dfs(grid, r, c);
+               }
+           }
+       }
+       return count;
+   }
+   private void dfs(char[][] grid, int r, int c) {
+       int row = grid.length;
+       int cols = grid[0].length;
+       if (r < 0 || c < 0 || r >= row || c >= cols || grid[r][c] == '0') {
+           return;
+       }
+       grid[r][c] = '0';
+       dfs(grid, r+1, c);
+       dfs(grid, r-1, c);
+       dfs(grid, r, c+1);
+       dfs(grid, r, c-1);
+   }
     static void main() {
         char[][] grid = {
                 {'1','1','1','1','0'},

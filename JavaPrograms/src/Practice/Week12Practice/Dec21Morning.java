d4fe63c06c41ff12268ea0acfb152c34699c03a5
SwarajXO <purarkarswaraj@gmail.com>
2026-01-22 06:32:31 +0530

    21-01-26 Array Practice

--- JavaPrograms/src/Practice/Week12Practice/Dec21Morning.java
+++ JavaPrograms/src/Practice/Week12Practice/Dec21Morning.java
@@ -4,21 +4,26 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Dec21Morning {
-    public static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[]{map.get(complement),i};
-            }
-            map.put(nums[i], i);
-        }
-        throw new IllegalArgumentException("Solution not found");
-    }
+    ///
 
-    static void main() {
-        int[] nums = {2,7,11,15};
-        int target = 9;
-        System.out.println(Arrays.toString(twoSum(nums,target)));
-    }
+
+
+    /// two Sum
+//    public static int[] twoSum(int[] nums, int target) {
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            int complement = target - nums[i];
+//            if (map.containsKey(complement)) {
+//                return new int[]{map.get(complement),i};
+//            }
+//            map.put(nums[i], i);
+//        }
+//        throw new IllegalArgumentException("Solution not found");
+//    }
+//
+//    static void main() {
+//        int[] nums = {2,7,11,15};
+//        int target = 9;
+//        System.out.println(Arrays.toString(twoSum(nums,target)));
+//    }
 }

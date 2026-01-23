a83a74d8b60ea8191c24437d54424d3d6496f5e2
SwarajXO <purarkarswaraj@gmail.com>
2026-01-23 09:08:38 +0530

    23-01-26 Array containsDuplicate, topKFrequent, ProductOfArrayExceptSelf practice

--- JavaPrograms/src/Practice/Week12Practice/Jan23Morning.java
+++ JavaPrograms/src/Practice/Week12Practice/Jan23Morning.java
@@ -6,26 +6,44 @@ import java.util.HashSet;
 import java.util.PriorityQueue;
 
 public class Jan23Morning {
-    public static int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        for (int x:nums){
-            freq.put(x, freq.getOrDefault(x, 0)+1);
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            ans[i] = prefix;
+            prefix *= nums[i];
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-                (a,b) -> freq.get(b) - freq.get(a)
-        );
-        pq.addAll(freq.keySet());
-        int[] ans = new int[k];
-        for (int i = 0; i < k; i++) {
-            ans[i] = pq.poll();
+         int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            ans[i] *= suffix;
+            suffix *= nums[i];
         } return ans;
     }
-
     static void main() {
-        int[] nums = {1,1,1,2,2,3};
-        int k = 2;
-        System.out.println(Arrays.toString(topKFrequent(nums,k)));
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
     }
+//    public static int[] topKFrequent(int[] nums, int k) {
+//        HashMap<Integer, Integer> freq = new HashMap<>();
+//        for (int x:nums){
+//            freq.put(x, freq.getOrDefault(x, 0)+1);
+//        }
+//        PriorityQueue<Integer> pq = new PriorityQueue<>(
+//                (a,b) -> freq.get(b) - freq.get(a)
+//        );
+//        pq.addAll(freq.keySet());
+//        int[] ans = new int[k];
+//        for (int i = 0; i < k; i++) {
+//            ans[i] = pq.poll();
+//        } return ans;
+//    }
+//
+//    static void main() {
+//        int[] nums = {1,1,1,2,2,3};
+//        int k = 2;
+//        System.out.println(Arrays.toString(topKFrequent(nums,k)));
+//    }
 //    public static boolean containsDuplicate(int[] nums) {
 //        HashSet<Integer> set = new HashSet<>();
 //        for (int x:nums) {

897d6966b6d59c305ff859a3f269fdcd432ffd28
SwarajXO <purarkarswaraj@gmail.com>
2026-01-23 08:51:26 +0530

    23-01-26 Array containsDuplicate, topKFrequent practice

--- JavaPrograms/src/Practice/Week12Practice/Jan23Morning.java
+++ JavaPrograms/src/Practice/Week12Practice/Jan23Morning.java
@@ -1,17 +1,40 @@
 package Practice.Week12Practice;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.PriorityQueue;
 
 public class Jan23Morning {
-    public static boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int x:nums) {
-            if (set.contains(x)) return true;
-            set.add(x);
-        } return false;
+    public static int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int x:nums){
+            freq.put(x, freq.getOrDefault(x, 0)+1);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                (a,b) -> freq.get(b) - freq.get(a)
+        );
+        pq.addAll(freq.keySet());
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll();
+        } return ans;
     }
+
     static void main() {
-        int[] nums = {1,2,3,1};
-        System.out.println(containsDuplicate(nums));
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums,k)));
     }
+//    public static boolean containsDuplicate(int[] nums) {
+//        HashSet<Integer> set = new HashSet<>();
+//        for (int x:nums) {
+//            if (set.contains(x)) return true;
+//            set.add(x);
+//        } return false;
+//    }
+//    static void main() {
+//        int[] nums = {1,2,3,1};
+//        System.out.println(containsDuplicate(nums));
+//    }
 }

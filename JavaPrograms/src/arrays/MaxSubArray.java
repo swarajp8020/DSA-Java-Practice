bc45fbcea8e74a571ec9d78ce25bd46da3059675
SwarajXO <purarkarswaraj@gmail.com>
2026-01-24 10:28:21 +0530

    24-01-26 MaxSubArray

--- JavaPrograms/src/arrays/MaxSubArray.java
+++ JavaPrograms/src/arrays/MaxSubArray.java
@@ -5,15 +5,12 @@ public class MaxSubArray {
     public static int maxSubArray(int[] nums) {
         int currSum = nums[0];
         int maxSum = nums[0];
-
         for (int i = 1; i < nums.length; i++) {
             currSum = Math.max(nums[i], currSum + nums[i]);
             maxSum = Math.max(maxSum, currSum);
         }
-
         return maxSum;
     }
-
     static void main(String[] args) {
         int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
         System.out.println(maxSubArray(nums));

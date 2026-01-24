b0a0041a0c58ce44e28481dadf1fe060d91cbd66
SwarajXO <purarkarswaraj@gmail.com>
2026-01-24 10:27:56 +0530

    24-01-26 MaxSubArray

---
+++ JavaPrograms/src/arrays/MaxSubArray.java
@@ -0,0 +1,21 @@
+package arrays;
+// LC 53 – Maximum Subarray (Kadane’s Algorithm) https://leetcode.com/problems/maximum-subarray/description/?envType=problem-list-v2&envId=rr2ss0g5
+/// At every index, decide: start fresh or extend previous subarray.
+public class MaxSubArray {
+    public static int maxSubArray(int[] nums) {
+        int currSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+
+        return maxSum;
+    }
+
+    static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(maxSubArray(nums));
+    }
+}

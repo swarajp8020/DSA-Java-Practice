000145c1e48cfdbd5c445d655a58d9584efe2722
SwarajXO <purarkarswaraj@gmail.com>
2026-01-24 08:45:43 +0530

    24-01-26 BestTimeToBuyAndSellStock, TopKFrequentElements practice

--- JavaPrograms/src/Practice/Week12Practice/Jan24Morning.java
+++ JavaPrograms/src/Practice/Week12Practice/Jan24Morning.java
@@ -1,19 +1,44 @@
 package Practice.Week12Practice;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
 public class Jan24Morning {
 
-    /// BestTimeToBuyAndSellStock
-    public static int maxProfit(int[] prices) {
-        int minPrices = Integer.MAX_VALUE;
-        int maxProfit = 0;
-        for (int p:prices) {
-            minPrices = Math.min(minPrices, p);
-            maxProfit = Math.max(maxProfit, p - minPrices);
+    /// TopKFrequentElements
+    public static int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int x:nums) {
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
         }
-        return maxProfit;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+                (a,b)->freq.get(b) - freq.get(a)
+        );
+        pq.addAll(freq.keySet());
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll();
+        }
+        return ans;
     }
-    static void main(String[] args) {
-        int[] prices = {7,1,5,3,6,4};
-        System.out.println(maxProfit(prices));
+    static void main() {
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums,k)));
     }
+    /// BestTimeToBuyAndSellStock
+//    public static int maxProfit(int[] prices) {
+//        int minPrices = Integer.MAX_VALUE;
+//        int maxProfit = 0;
+//        for (int p:prices) {
+//            minPrices = Math.min(minPrices, p);
+//            maxProfit = Math.max(maxProfit, p - minPrices);
+//        }
+//        return maxProfit;
+//    }
+//    static void main(String[] args) {
+//        int[] prices = {7,1,5,3,6,4};
+//        System.out.println(maxProfit(prices));
+//    }
 }

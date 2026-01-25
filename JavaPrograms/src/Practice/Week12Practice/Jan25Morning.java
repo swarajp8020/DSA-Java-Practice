8e2dc9a9a38701cead3eb9b230e0a7aefae9a1f4
SwarajXO <purarkarswaraj@gmail.com>
2026-01-25 19:19:32 +0530

    25-01-26 minSubArrayLen practice

--- JavaPrograms/src/Practice/Week12Practice/Jan25Morning.java
+++ JavaPrograms/src/Practice/Week12Practice/Jan25Morning.java
@@ -1,22 +1,47 @@
 package Practice.Week12Practice;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Jan25Morning {
-
-    // Min Size
-    public static int minSubArrayLen(int target, int[] nums) {
-        int left = 0, sum = 0, minLen = Integer.MAX_VALUE;
-        for (int right = 0; right < nums.length; right++) {
-            sum += nums[right];
-            while (sum >= target) {
-                minLen = Math.min(minLen, right - left + 1);
-                sum -= nums[left++];
+    /// Longest Repeating Character Replacement
+    public static int characterReplacement(String s, String k) {
+        int[] freq = new int[26];
+        int left =0, maxFreq = 0, maxLen = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            freq[c-'a']++;
+            maxFreq = Math.max(maxFreq, freq[c-'a']);
+            int windowSize = right-left+1;
+            int charsToChange = windowSize-maxFreq;
+            if (charsToChange > k) {
+                freq[s.charAt(left)-'a']--;
+                left++;
             }
-        } return minLen == Integer.MAX_VALUE ? 0 : minLen;
+            maxLen = Math.max(maxLen, right-left+1);
+        }
+        return maxLen;
     }
-
-    static void main() {
-        int[] nums = {2,3,1,2,4,3};
-        int target = 7;
-        System.out.println(minSubArrayLen(target, nums));
+    static void main(String[] args) {
+        String s = "sada";
+        int k = 2;
+        System.out.println(characterReplacement(s,k));
     }
+    // Min Size
+//    public static int minSubArrayLen(int target, int[] nums) {
+//        int left = 0, sum = 0, minLen = Integer.MAX_VALUE;
+//        for (int right = 0; right < nums.length; right++) {
+//            sum += nums[right];
+//            while (sum >= target) {
+//                minLen = Math.min(minLen, right - left + 1);
+//                sum -= nums[left++];
+//            }
+//        } return minLen == Integer.MAX_VALUE ? 0 : minLen;
+//    }
+//
+//    static void main() {
+//        int[] nums = {2,3,1,2,4,3};
+//        int target = 7;
+//        System.out.println(minSubArrayLen(target, nums));
+//    }
 }

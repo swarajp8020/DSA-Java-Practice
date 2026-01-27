8e1e9ff74667540f1c561fb18cdc4502c6b0eb59
SwarajXO <purarkarswaraj@gmail.com>
2026-01-27 08:27:04 +0530

    27-01-26 MergeSortedArray practice

--- JavaPrograms/src/Practice/Week13Practice/Jan27Morning.java
+++ JavaPrograms/src/Practice/Week13Practice/Jan27Morning.java
@@ -7,11 +7,8 @@ public class Jan27Morning {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int i = m - 1, j = n - 1, k = m + n - 1;
         while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
-            }
+            if (i >= 0 && nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
         }
     }
     static void main() {

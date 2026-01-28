7d1712abed368db56d9547bbac7f637ad3b60fa5
SwarajXO <purarkarswaraj@gmail.com>
2026-01-28 07:08:34 +0530

    28-01-26 twoSum2 practice

---
+++ JavaPrograms/src/Practice/Week13Practice/Jan28Morning.java
@@ -0,0 +1,22 @@
+package Practice.Week13Practice;
+
+import java.util.Arrays;
+
+public class Jan28Morning {
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) left++;
+            else right--;
+        }
+        return new int[]{-1,-1};
+    }
+    static void main(String[] args) {
+        int[] numbers = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+}

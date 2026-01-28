782de594e3ceb4a1449e38c8dfe29eecf98f91e7
SwarajXO <purarkarswaraj@gmail.com>
2026-01-28 07:31:25 +0530

    28-01-26 missingNumber practice

--- JavaPrograms/src/Practice/Week13Practice/Jan28Morning.java
+++ JavaPrograms/src/Practice/Week13Practice/Jan28Morning.java
@@ -3,19 +3,33 @@ package Practice.Week13Practice;
 import java.util.Arrays;
 
 public class Jan28Morning {
-    public static int[] twoSum(int[] numbers, int target) {
-        int left = 0, right = numbers.length - 1;
-        while (left < right) {
-            int sum = numbers[left] + numbers[right];
-            if (sum == target) {
-                return new int[]{left + 1, right + 1};
-            } else if (sum < target) left++;
-            else right--;
-        } return new int[]{-1, -1};
+    public static int missingNumber(int[] nums) {
+        int res = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            res ^= i;
+        }
+        for (int x:nums){
+            res ^= x;
+        } return res;
     }
-    static void main(String[] args) {
-        int[] numbers = {2,7,11,15};
-        int target = 9;
-        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    static void main() {
+        int[] nums = {3,0,1};
+        System.out.println(missingNumber(nums)                                                                          );
     }
+    /// twoSum2
+//    public static int[] twoSum(int[] numbers, int target) {
+//        int left = 0, right = numbers.length - 1;
+//        while (left < right) {
+//            int sum = numbers[left] + numbers[right];
+//            if (sum == target) {
+//                return new int[]{left + 1, right + 1};
+//            } else if (sum < target) left++;
+//            else right--;
+//        } return new int[]{-1, -1};
+//    }
+//    static void main(String[] args) {
+//        int[] numbers = {2,7,11,15};
+//        int target = 9;
+//        System.out.println(Arrays.toString(twoSum(numbers, target)));
+//    }
 }

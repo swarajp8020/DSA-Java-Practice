7d8790188810e2f4c42068bae39ff76446065473
SwarajXO <purarkarswaraj@gmail.com>
2026-01-29 09:07:16 +0530

    29-01-26 climbingStairs practice

---
+++ JavaPrograms/src/Practice/Week13Practice/Jan29Morning.java
@@ -0,0 +1,17 @@
+package Practice.Week13Practice;
+
+public class Jan29Morning {
+    public static int climbStairs(int n){
+        if (n <= 2) return n;
+        int a = 1, b = 1;
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        } return b;
+    }
+    static void main(String[] args) {
+        int n = 3;
+        System.out.println(climbStairs(n));
+    }
+}

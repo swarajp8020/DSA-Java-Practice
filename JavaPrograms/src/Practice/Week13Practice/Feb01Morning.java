3a243a373b9129c021a58b54759d61aa42457e58
SwarajXO <purarkarswaraj@gmail.com>
2026-02-01 07:34:25 +0530

    01-02-26 threeSum practice

---
+++ JavaPrograms/src/Practice/Week13Practice/Feb01Morning.java
@@ -0,0 +1,32 @@
+package Practice.Week13Practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Feb01Morning {
+    public static List<List<Integer>> threeSum(int[] nums){
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            int left = i + 1, right = nums.length-1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left<right&&nums[left]==nums[left-1]) left++;
+                    while (left<right&&nums[right]==nums[right+1]) right--;
+                }
+                else if (sum<0)left++;
+                else right--;
+            }
+        } return res;
+    }
+    static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum(nums));
+    }
+}

414238414e05d18bcd55ee8239efab5100c6c2c0
SwarajXO <purarkarswaraj@gmail.com>
2026-02-01 07:51:09 +0530

    01-02-26 threeSum practice

--- JavaPrograms/src/Practice/Week13Practice/Feb01Morning.java
+++ JavaPrograms/src/Practice/Week13Practice/Feb01Morning.java
@@ -9,18 +9,17 @@ public class Feb01Morning {
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            if (i>0 && nums[i]==nums[i-1]) continue;
-            int left = i + 1, right = nums.length-1;
+            if (i > 0 && nums[i] == nums[ i - 1]) continue;
+            int left = i + 1, right = nums.length - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
                     res.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
-                    while (left<right&&nums[left]==nums[left-1]) left++;
-                    while (left<right&&nums[right]==nums[right+1]) right--;
-                }
-                else if (sum<0)left++;
+                    while (left < right && nums[left] == nums[left-1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum<0) left++;
                 else right--;
             }
         } return res;

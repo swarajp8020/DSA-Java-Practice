06bccec292a546599f0a2778c609d5acd906e93d
SwarajXO <purarkarswaraj@gmail.com>
2026-02-03 07:41:54 +0530

    03-02-26 JumpGame practice

--- JavaPrograms/src/Practice/Week13Practice/Feb03Morning.java
+++ JavaPrograms/src/Practice/Week13Practice/Feb03Morning.java
@@ -4,9 +4,7 @@ public class Feb03Morning {
     public static boolean canJump(int[] nums) {
         int farthest = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (i > farthest) {
-                return false;
-            }
+            if (i > farthest) return false;
             farthest = Math.max(farthest, i + nums[i]);
         }
         return true;

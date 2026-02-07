c3eaf4727d71fb0b7a77d44b12a80b1b2cae7e06
SwarajXO <purarkarswaraj@gmail.com>
2026-02-07 09:58:15 +0530

    07-02-26 MedianFinder

---
+++ JavaPrograms/src/arrays/MedianFinder.java
@@ -0,0 +1,45 @@
+package arrays;
+
+import java.util.PriorityQueue;
+
+public class MedianFinder {
+    PriorityQueue<Integer> maxHeap;
+    PriorityQueue<Integer> minHeap;
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        minHeap = new PriorityQueue<>();
+    }
+    public void addNum(int num) {
+        if(maxHeap.isEmpty() || num <= maxHeap.peek()) {
+            maxHeap.add(num);
+        } else {
+            minHeap.add(num);
+        }
+        // balance heaps
+        if(maxHeap.size() > minHeap.size() + 1) {
+            minHeap.add(maxHeap.poll());
+        }
+        else if(minHeap.size() > maxHeap.size()) {
+            maxHeap.add(minHeap.poll());
+        }
+    }
+    public double findMedian() {
+        if(maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
+        return maxHeap.peek();
+    }
+
+    static void main(String[] args) {
+        MedianFinder mf = new MedianFinder();
+
+        int[] nums = {5, 15, 1, 3};
+
+        for(int num : nums) {
+            mf.addNum(num);
+            System.out.println("Added: " + num);
+            System.out.println("Current Median: " + mf.findMedian());
+            System.out.println("---------------------");
+        }
+    }
+}

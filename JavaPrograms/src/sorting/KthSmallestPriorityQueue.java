49ed4623b18fa0bd9c3f00ad074953861f1c4add
SwarajXO <purarkarswaraj@gmail.com>
2026-02-08 08:49:24 +0530

    08-02-26 kthSmallest

---
+++ JavaPrograms/src/sorting/KthSmallestPriorityQueue.java
@@ -0,0 +1,49 @@
+package sorting;
+
+import java.util.PriorityQueue;
+/// ELI5 Explanation
+/// The matrix is already partially sorted.
+/// Instead of flattening and sorting everything,
+/// we smartly take advantage of the sorted rows.
+/// We treat each row like a sorted list and merge them using a heap.
+/// Exactly like “merge k sorted lists”.
+/// Pattern Identification
+/// Whenever you see:
+/// • sorted matrix
+/// • kth smallest
+/// • multiple sorted rows
+/// Think:
+/// 👉 Min Heap of size k
+public class KthSmallestPriorityQueue {
+    public static int kthSmallest(int[][] matrix, int k) {
+        PriorityQueue<int[]> minHeap =
+                new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        int n = matrix.length;
+        for(int i = 0; i < n; i++) {
+            minHeap.add(new int[]{matrix[i][0], i, 0});
+        }
+        int result = 0;
+        while(k-- > 0) {
+            int[] curr = minHeap.poll();
+            result = curr[0];
+            int row = curr[1];
+            int col = curr[2];
+            if(col + 1 < n) {
+                minHeap.add(new int[]{
+                        matrix[row][col + 1], row, col + 1
+                });
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 5, 9},
+                {10, 11, 13},
+                {12, 13, 15}
+        };
+        int k =2;
+        int result = kthSmallest(matrix, k);
+        System.out.println("Kth Smallest Element: " + result);
+    }
+}

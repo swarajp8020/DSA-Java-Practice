923908211c588cc3c247a30d5edeb8976760e812
SwarajXO <purarkarswaraj@gmail.com>
2026-02-09 08:48:48 +0530

    09-02-26 topKFrequent practice

--- JavaPrograms/src/Practice/Week15Practice/Dec09Morning.java
+++ JavaPrograms/src/Practice/Week15Practice/Dec09Morning.java
@@ -1,22 +1,52 @@
 package Practice.Week15Practice;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class Dec09Morning {
-    //Kth Largest Element in an Array
-    public static int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    //TopKFrequent
+    public static int[] topKFrequent(int[] nums, int k){
+        Map<Integer, Integer> map = new HashMap<>();
         for (int num: nums) {
-            minHeap.add(num);
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(
+                (a,b) -> map.get(a) - map.get(b)
+        );
+        for (int key: map.keySet()) {
+            minHeap.add(key);
             if (minHeap.size() > k) {
                 minHeap.poll();
             }
-        } return minHeap.peek();
+        }
+        int[] result = new int[k];
+        int i = 0;
+        while (!minHeap.isEmpty()) {
+            result[i++] = minHeap.poll();
+        }
+        return result;
     }
-
-    static void main(String[] args) {
-        int[] arr = {8,5,7,2,9,3};
-        int k = 3;
-        System.out.println(findKthLargest(arr, k));
+    static void main() {
+        int[] nums = {1,1,1,2,2,3};
+        int k = 2;
+        System.out.println(Arrays.toString(topKFrequent(nums,k)));
     }
+    //Kth Largest Element in an Array
+//    public static int findKthLargest(int[] nums, int k) {
+//        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+//        for (int num: nums) {
+//            minHeap.add(num);
+//            if (minHeap.size() > k) {
+//                minHeap.poll();
+//            }
+//        } return minHeap.peek();
+//    }
+//
+//    static void main(String[] args) {
+//        int[] arr = {8,5,7,2,9,3};
+//        int k = 3;
+//        System.out.println(findKthLargest(arr, k));
+//    }
 }

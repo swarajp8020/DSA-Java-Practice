8d69b77f28d6016c5bd59bae27e196e68e7b0476
SwarajXO <purarkarswaraj@gmail.com>
2026-02-13 09:01:10 +0530

    13-02-26 Subsets Practice

---
+++ JavaPrograms/src/Practice/Week15Practice/Feb13Morning.java
@@ -0,0 +1,32 @@
+package Practice.Week15Practice;
+
+import priorityQueue.Subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Feb13Morning {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+        backtrack(nums, 0, subset, result);
+        return result;
+    }
+    private void backtrack(int[] nums, int start,
+                           List<Integer> subset,
+                           List<List<Integer>> result) {
+        result.add(new ArrayList<>(subset));
+        for(int i = start; i < nums.length; i++) {
+            subset.add(nums[i]);
+            backtrack(nums, i + 1, subset, result);
+            subset.remove(subset.size() - 1);
+        }
+    }
+    public static void main(String[] args) {
+
+        Feb13Morning s = new Feb13Morning();
+        int[] nums = {1,2,3};
+        List<List<Integer>> res = s.subsets(nums);
+        System.out.println(res);
+    }
+}

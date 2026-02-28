bf92c71e757f5bbfef4a160746f8fdd10f5f5190
SwarajXO <purarkarswaraj@gmail.com>
2026-02-28 08:36:09 +0530

    27-02-26 ShortestPathBinaryMatrix

--- JavaPrograms/src/priorityQueue/ShortestPathBinaryMatrix.java
+++ JavaPrograms/src/priorityQueue/ShortestPathBinaryMatrix.java
@@ -47,24 +47,17 @@ import java.util.Queue;
 
 public class ShortestPathBinaryMatrix {
     public int shortestPathBinaryMatrix(int[][] grid) {
-
         int n = grid.length;
-
         if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
             return -1;
-
         int[][] directions = {
                 {1,0},{-1,0},{0,1},{0,-1},
                 {1,1},{1,-1},{-1,1},{-1,-1}
         };
-
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0, 1});
-
         grid[0][0] = 1; // mark visited
-
         while(!queue.isEmpty()) {
-
             int[] curr = queue.poll();
             int r = curr[0];
             int c = curr[1];
@@ -87,14 +80,11 @@ public class ShortestPathBinaryMatrix {
         return -1;
     }
     static void main(String[] args) {
-
         ShortestPathBinaryMatrix s = new ShortestPathBinaryMatrix();
-
         int[][] grid = {
                 {0,1},
                 {1,0}
         };
-
         System.out.println("Shortest Path: "
                 + s.shortestPathBinaryMatrix(grid));
     }

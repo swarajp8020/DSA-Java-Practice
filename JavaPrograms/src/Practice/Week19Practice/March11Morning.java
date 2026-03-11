10e5eaf9baeef8d31280924cc6358c074d74a6a2
SwarajXO <purarkarswaraj@gmail.com>
2026-03-11 08:48:26 +0530

    11-03-26 dailyTemperatures

--- JavaPrograms/src/Practice/Week19Practice/March11Morning.java
+++ JavaPrograms/src/Practice/Week19Practice/March11Morning.java
@@ -5,14 +5,10 @@ import java.util.Stack;
 
 public class March11Morning {
     public int[] dailyTemperatures(int[] temperatures) {
-
         int n = temperatures.length;
         int[] result = new int[n];
-
         Stack<Integer> stack = new Stack<>();
-
         for(int i = 0; i < n; i++) {
-
             while(!stack.isEmpty() &&
                     temperatures[i] > temperatures[stack.peek()]) {
                 int prevIndex = stack.pop();
@@ -22,7 +18,7 @@ public class March11Morning {
         }
         return result;
     }
-    public static void main(String[] args) {
+    static void main(String[] args) {
         March11Morning sol = new March11Morning();
         int[] temps = {73,74,75,71,69,72,76,73};
         int[] result = sol.dailyTemperatures(temps);

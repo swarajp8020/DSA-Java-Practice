15f10b73e9ad0c4f8c9a52af13179d76ca1821ff
SwarajXO <purarkarswaraj@gmail.com>
2026-03-14 07:40:55 +0530

    14-03-26 LRU Cache

--- JavaPrograms/src/Practice/Week19Practice/March14Morning.java
+++ JavaPrograms/src/Practice/Week19Practice/March14Morning.java
@@ -13,80 +13,54 @@ public class March14Morning {
             value = v;
         }
     }
-
     private int capacity;
     private Map<Integer, Node> map;
-
     private Node head, tail;
-
     public March14Morning(int capacity) {
         this.capacity = capacity;
         map = new HashMap<>();
-
         head = new Node(0,0);
         tail = new Node(0,0);
-
         head.next = tail;
         tail.prev = head;
     }
-
     private void remove(Node node) {
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
-
     private void insert(Node node) {
         node.next = head.next;
         node.prev = head;
-
         head.next.prev = node;
         head.next = node;
     }
-
     public int get(int key) {
-
         if(!map.containsKey(key))
             return -1;
-
         Node node = map.get(key);
-
         remove(node);
         insert(node);
-
         return node.value;
     }
-
     public void put(int key, int value) {
-
         if(map.containsKey(key)) {
             remove(map.get(key));
         }
-
         Node node = new Node(key,value);
-
         insert(node);
         map.put(key,node);
-
         if(map.size() > capacity) {
-
             Node lru = tail.prev;
-
             remove(lru);
             map.remove(lru.key);
         }
     }
-
-    public static void main(String[] args) {
-
+    static void main(String[] args) {
         March14Morning cache = new March14Morning(2);
-
         cache.put(1,1);
         cache.put(2,2);
-
         System.out.println(cache.get(1)); // 1
-
         cache.put(3,3);
-
         System.out.println(cache.get(2)); // -1
     }
 }

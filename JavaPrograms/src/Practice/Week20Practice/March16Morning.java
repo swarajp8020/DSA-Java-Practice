953afe9c6d3a51c173ab2d16a7b26d4cee950b84
SwarajXO <purarkarswaraj@gmail.com>
2026-03-16 08:34:45 +0530

    16-03-26 Minimum Window Substring

--- JavaPrograms/src/Practice/Week20Practice/March16Morning.java
+++ JavaPrograms/src/Practice/Week20Practice/March16Morning.java
@@ -5,90 +5,64 @@ import java.util.Map;
 
 public class March16Morning {
     public static String minWindow(String sourceString, String targetString) {
-
         if (sourceString.length() < targetString.length()) {
             return "";
         }
-
         Map<Character, Integer> targetFrequencyMap = new HashMap<>();
-
         for (char character : targetString.toCharArray()) {
             targetFrequencyMap.put(
                     character,
                     targetFrequencyMap.getOrDefault(character, 0) + 1
             );
         }
-
         Map<Character, Integer> windowFrequencyMap = new HashMap<>();
-
         int have = 0;
         int need = targetFrequencyMap.size();
-
         int leftPointer = 0;
-
         int minimumWindowLength = Integer.MAX_VALUE;
         int resultStartIndex = 0;
-
         for (int rightPointer = 0; rightPointer < sourceString.length(); rightPointer++) {
-
             char currentCharacter = sourceString.charAt(rightPointer);
-
             windowFrequencyMap.put(
                     currentCharacter,
                     windowFrequencyMap.getOrDefault(currentCharacter, 0) + 1
             );
-
             if (targetFrequencyMap.containsKey(currentCharacter) &&
                     windowFrequencyMap.get(currentCharacter).intValue() ==
                             targetFrequencyMap.get(currentCharacter).intValue()) {
-
                 have++;
             }
-
             while (have == need) {
-
                 int currentWindowLength = rightPointer - leftPointer + 1;
-
                 if (currentWindowLength < minimumWindowLength) {
                     minimumWindowLength = currentWindowLength;
                     resultStartIndex = leftPointer;
                 }
-
                 char leftCharacter = sourceString.charAt(leftPointer);
-
                 windowFrequencyMap.put(
                         leftCharacter,
                         windowFrequencyMap.get(leftCharacter) - 1
                 );
-
                 if (targetFrequencyMap.containsKey(leftCharacter) &&
                         windowFrequencyMap.get(leftCharacter) <
                                 targetFrequencyMap.get(leftCharacter)) {
-
                     have--;
                 }
-
                 leftPointer++;
             }
         }
-
         if (minimumWindowLength == Integer.MAX_VALUE) {
             return "";
         }
-
         return sourceString.substring(
                 resultStartIndex,
                 resultStartIndex + minimumWindowLength
         );
     }
-
-    public static void main(String[] args) {
-
+    static void main(String[] args) {
         String sourceString = "ADOBECODEBANC";
         String targetString = "ABC";
-
         String result = minWindow(sourceString, targetString);
-
         System.out.println("Minimum Window: " + result);
     }
 }

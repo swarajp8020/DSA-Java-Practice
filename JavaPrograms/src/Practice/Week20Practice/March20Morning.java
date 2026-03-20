e74413de13233643a2e276f8b2c00b42e27de8ff
SwarajXO <purarkarswaraj@gmail.com>
2026-03-20 08:25:35 +0530

    20-03-26 Course Schedule

--- JavaPrograms/src/Practice/Week20Practice/March20Morning.java
+++ JavaPrograms/src/Practice/Week20Practice/March20Morning.java
@@ -8,57 +8,39 @@ import java.util.Queue;
 /// Course Schedule
 public class March20Morning {
     public static boolean canFinish(int totalCourses, int[][] prerequisites) {
-
         List<List<Integer>> adjacencyList = new ArrayList<>();
-
         for (int i = 0; i < totalCourses; i++) {
             adjacencyList.add(new ArrayList<>());
         }
-
         int[] indegreeArray = new int[totalCourses];
-
         for (int[] prerequisite : prerequisites) {
-
             int course = prerequisite[0];
             int dependency = prerequisite[1];
-
             adjacencyList.get(dependency).add(course);
             indegreeArray[course]++;
         }
-
         Queue<Integer> processingQueue = new LinkedList<>();
-
         for (int course = 0; course < totalCourses; course++) {
             if (indegreeArray[course] == 0) {
                 processingQueue.offer(course);
             }
         }
-
         int completedCourses = 0;
-
         while (!processingQueue.isEmpty()) {
-
             int currentCourse = processingQueue.poll();
             completedCourses++;
-
             for (int neighborCourse : adjacencyList.get(currentCourse)) {
-
                 indegreeArray[neighborCourse]--;
-
                 if (indegreeArray[neighborCourse] == 0) {
                     processingQueue.offer(neighborCourse);
                 }
             }
         }
-
         return completedCourses == totalCourses;
     }
-
     public static void main(String[] args) {
-
         int totalCourses = 2;
         int[][] prerequisites = {{1,0}};
-
         System.out.println(canFinish(totalCourses, prerequisites)); // true
     }
 }

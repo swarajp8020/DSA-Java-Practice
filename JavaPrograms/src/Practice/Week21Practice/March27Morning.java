cc2a688e8ed4266d8e460f9639138b3971674dc1
SwarajXO <purarkarswaraj@gmail.com>
2026-03-27 09:20:46 +0530

    27-03-26 Number of Islands

--- JavaPrograms/src/Practice/Week21Practice/March27Morning.java
+++ JavaPrograms/src/Practice/Week21Practice/March27Morning.java
@@ -2,50 +2,36 @@ package Practice.Week21Practice;
 /// Number of Islands
 public class March27Morning {
     public static int numIslands(char[][] grid) {
-
         int islandCount = 0;
-
         for (int row = 0; row < grid.length; row++) {
-
             for (int col = 0; col < grid[0].length; col++) {
-
                 if (grid[row][col] == '1') {
-
                     islandCount++;
-
                     exploreIsland(grid, row, col);
                 }
             }
         }
-
         return islandCount;
     }
-
     private static void exploreIsland(char[][] grid, int row, int col) {
-
         if (row < 0 || col < 0 ||
                 row >= grid.length || col >= grid[0].length ||
                 grid[row][col] == '0') {
             return;
         }
-
         grid[row][col] = '0'; // mark visited
-
         exploreIsland(grid, row + 1, col);
         exploreIsland(grid, row - 1, col);
         exploreIsland(grid, row, col + 1);
         exploreIsland(grid, row, col - 1);
     }
-
-    public static void main(String[] args) {
-
+    static void main(String[] args) {
         char[][] grid = {
                 {'1','1','0','0'},
                 {'1','1','0','0'},
                 {'0','0','1','0'},
                 {'0','0','0','1'}
         };
-
         System.out.println(numIslands(grid)); // 3
     }
 }

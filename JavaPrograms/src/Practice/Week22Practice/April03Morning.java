1354915071ea2a78494f42b7d71e2531869d0f40
SwarajXO <purarkarswaraj@gmail.com>
2026-04-03 09:25:14 +0530

    03-04-26 Valid Parentheses

--- JavaPrograms/src/Practice/Week22Practice/April03Morning.java
+++ JavaPrograms/src/Practice/Week22Practice/April03Morning.java
@@ -5,37 +5,26 @@ import java.util.Stack;
 /// Valid Parentheses
 public class April03Morning {
     public static boolean isValid(String inputString) {
-
         Stack<Character> bracketStack = new Stack<>();
-
         for (char currentChar : inputString.toCharArray()) {
-
             if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
                 bracketStack.push(currentChar);
             } else {
-
                 if (bracketStack.isEmpty()) {
                     return false;
                 }
-
                 char topElement = bracketStack.pop();
-
                 if ((currentChar == ')' && topElement != '(') ||
                         (currentChar == ']' && topElement != '[') ||
                         (currentChar == '}' && topElement != '{')) {
-
                     return false;
                 }
             }
         }
-
         return bracketStack.isEmpty();
     }
-
-    public static void main(String[] args) {
-
+    static void main(String[] args) {
         String inputString = "()[]{}";
-
         System.out.println(isValid(inputString)); // true
     }
 }
